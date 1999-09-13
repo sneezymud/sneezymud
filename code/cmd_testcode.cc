de704802ae85c48828ce868cb5e64fb9c8d98c83
batopr <batopr@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
1999-09-13 14:27:25 +0000

    *** empty log message ***

--- code/cmd_testcode.cc
+++ code/cmd_testcode.cc
@@ -3,6 +3,9 @@
 // SneezyMUD - All rights reserved, SneezyMUD Coding Team
 //
 // $Log: cmd_testcode.cc,v $
+// Revision 1.2  1999/09/13 14:27:25  batopr
+// *** empty log message ***
+//
 // Revision 1.1  1999/09/12 17:24:04  sneezy
 // Initial revision
 //
@@ -96,7 +99,7 @@ void TBeing::doTestCode(const char *arg)
   sendTo("2: '%s'\n\r", str2.c_str());
   sendTo("ret: '%s'\n\r", strret.c_str());
 #endif
-#if 0
+#if 1
   extern void rewrite_obj_file();
   rewrite_obj_file();
   return;
