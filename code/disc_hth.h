5c33481b49e72ec732f51d5e01f3659623aea3bc
jesus <jesus@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2000-09-29 15:46:10 +0000

    This commit was generated by cvs2svn to compensate for changes in r1870, which included commits to RCS files with non-trunk default branches.

--- code/disc_hth.h
+++ code/disc_hth.h
@@ -3,6 +3,9 @@
 // SneezyMUD - All rights reserved, SneezyMUD Coding Team
 //
 // $Log: disc_hth.h,v $
+// Revision 5.1.1.2  2000/09/29 15:45:27  jesus
+// added warrior parry skill
+//
 // Revision 5.1.1.1  1999/10/16 04:32:20  batopr
 // new branch
 //
@@ -26,12 +29,33 @@ class CDHTH : public CDiscipline
 public:
     CSkill skShove;
     CSkill skRetreat;
+    CSkill skParryWarrior;
 
-    CDHTH();
-    CDHTH(const CDHTH &a);
-    CDHTH & operator=(const CDHTH &a);
-    virtual ~CDHTH();
-    virtual CDHTH * cloneMe();
+    CDHTH()
+      : CDiscipline(),
+      skShove(),
+      skRetreat(),
+      skParryWarrior() {
+    }      
+    CDHTH(const CDHTH &a)
+      : CDiscipline(a),
+      skShove(a.skShove),
+      skRetreat(a.skRetreat),
+      skParryWarrior(a.skParryWarrior) {
+    }
+    CDHTH & operator=(const CDHTH &a) {
+      if (this == &a) return *this;
+      CDiscipline::operator=(a);
+      skShove = a.skShove;
+      skRetreat = a.skRetreat;
+      skParryWarrior = a.skParryWarrior;
+      return *this;
+    } 
+    //    CDHTH();
+    //    CDHTH(const CDHTH &a);
+    //    CDHTH & operator=(const CDHTH &a);
+    virtual ~CDHTH() {}
+    virtual CDHTH * cloneMe() { return new CDHTH(*this); }
 private:
 };
 
