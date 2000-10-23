cd894953cc266df575ac0dce0612254324c39c60
jesus <jesus@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2000-10-23 05:59:43 +0000

    This commit was generated by cvs2svn to compensate for changes in r1938, which included commits to RCS files with non-trunk default branches.

--- code/disc_brawling.h
+++ code/disc_brawling.h
@@ -3,6 +3,9 @@
 // SneezyMUD - All rights reserved, SneezyMUD Coding Team
 //
 // $Log: disc_brawling.h,v $
+// Revision 5.1.1.2  2000/10/23 05:55:41  jesus
+// added spin skill
+//
 // Revision 5.1.1.1  1999/10/16 04:32:20  batopr
 // new branch
 //
@@ -27,6 +30,7 @@ class CDBrawling : public CDiscipline
     CSkill skGrapple;
     CSkill skStomp;
     CSkill skBodyslam;
+    CSkill skSpin;
 
     CDBrawling();
     CDBrawling(const CDBrawling &a);
@@ -38,4 +42,11 @@ class CDBrawling : public CDiscipline
 };
 
 #endif
+
+
+
+
+
+
+
 
