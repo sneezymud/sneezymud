6914e83af7da539714b4c7b15b65633a0ac59dfa
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2001-06-11 20:53:57 +0000

    added zone name search fixed bug

--- code/cmd_trophy.cc
+++ code/cmd_trophy.cc
@@ -44,9 +44,10 @@ const char *describe_trophy_exp(float count)
 
 void TBeing::doTrophy(const char *arg)
 {
-  MYSQL_ROW row;
+  MYSQL_ROW row=NULL;
   MYSQL_RES *res;
   int rc, mcount=0, vnum, header=0, zcount=0, bottom=0, zcountt=0;
+  int zonesearch=0, processedrow=1;
   float count;
   char buf[256];
   string sb;
@@ -58,24 +59,50 @@ void TBeing::doTrophy(const char *arg)
   }
 
   for (; isspace(*arg); arg++);
-
+  
+  if(!strncmp(arg, "zone", 4)){
+    zonesearch=1;
+    for (; !isspace(*arg); arg++);
+  }
 
   rc=dbquery(&res, "sneezy", "doTrophy", "select mobvnum, count from trophy where name='%s' order by mobvnum", getName());
 
   for (zone = 0; zone < zone_table.size(); zone++) {
     zoneData &zd = zone_table[zone];
     
-    while((row=mysql_fetch_row(res))){
+    while(1){
+      if(processedrow)
+	row=mysql_fetchrow(res);
+
+      if(!row)
+	break;
+
+      processedrow=0;
       vlogf(LOG_PEEL, "mob=%i, top=%i", atoi(row[0]), zd.top);
 
       // sometimes we get an entry of 0 for med mobs I think
       vnum=atoi(row[0]);
       if(vnum==0)
 	continue;
-      
+
       // this mob doesn't belong to this zone, so break out to the zone loop
       if(vnum>zd.top)
 	break;
+
+      int rnum = real_mobile(atoi(row[0]));
+      if (rnum < 0) {
+	vlogf(LOG_BUG, "DoTrophy detected bad mobvnum=%d for name='%s'", 
+	      atoi(row[0]), getName());
+	continue;
+      }
+
+      if(zonesearch){
+	if(*arg && !isname(arg, zd.name))
+	  continue;
+      } else {
+	if(*arg && !isname(arg, mob_index[rnum].name))
+	  continue;
+      }
 
       // print the zone header if we haven't already
       // we do it here, so we can prevent printing headers for empty zones
@@ -85,16 +112,6 @@ void TBeing::doTrophy(const char *arg)
 	header=1;
       }
 
-      int rnum = real_mobile(atoi(row[0]));
-      if (rnum < 0) {
-	vlogf(LOG_BUG, "DoTrophy detected bad mobvnum=%d for name='%s'", 
-	      atoi(row[0]), getName());
-	continue;
-      }
-      
-      if(*arg && !isname(arg, mob_index[rnum].name))
-	continue;
-      
       count=atof(row[1]);
       sprintf(buf, "You will gain %s experience when fighting %s.\n\r", 
 	      describe_trophy_exp(count),
@@ -102,7 +119,8 @@ void TBeing::doTrophy(const char *arg)
       ++mcount;
       ++zcount;
       sb += buf;
-      
+
+      processedrow=1;
       
 #if 0
     sprintf(buf, "%3d %-38.38s %4dm %4dm %6d-%-6d %3d %.1f\n\r", 
