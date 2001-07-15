3ed23a10ce6c56e5e6f1bf46683a5d2df53cff10
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2001-07-15 21:10:15 +0000

    added disc_defense

---
+++ code/disc_defense.cc
@@ -0,0 +1,33 @@
+//////////////////////////////////////////////////////////////////////////
+//
+// SneezyMUD - All rights reserved, SneezyMUD Coding Team
+//
+//////////////////////////////////////////////////////////////////////////
+
+
+#include "stdsneezy.h"
+#include "disc_defense.h"
+
+CDDefense::CDDefense() :
+  CDiscipline(),
+  skAdvancedDefense()
+{
+}
+
+CDDefense::CDDefense(const CDDefense &a) :
+  CDiscipline(a),
+  skAdvancedDefense(a.skAdvancedDefense)
+{
+}
+
+CDDefense & CDDefense::operator=(const CDDefense &a)
+{
+  if (this == &a) return *this;
+  CDiscipline::operator=(a);
+  skAdvancedDefense = a.skAdvancedDefense;
+  return *this;
+}
+
+CDDefense::~CDDefense()
+{
+}
---
+++ code/disc_defense.h
@@ -0,0 +1,26 @@
+//////////////////////////////////////////////////////////////////////////
+//
+// SneezyMUD - All rights reserved, SneezyMUD Coding Team
+//
+//////////////////////////////////////////////////////////////////////////
+
+
+#ifndef __DISC_DEFENSE_H
+#define __DISC_DEFENSE_H
+
+class CDDefense : public CDiscipline
+{
+public:
+    CSkill skAdvancedDefense;
+
+    CDDefense();
+    CDDefense(const CDDefense &a);
+    CDDefense & operator=(const CDDefense &a);
+    virtual ~CDDefense();
+    virtual CDDefense * cloneMe() { return new CDDefense(*this); }
+
+private:
+};
+
+#endif
+
