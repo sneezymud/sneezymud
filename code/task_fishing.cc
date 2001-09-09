e1c10625d278c2508f9643237ddaae3f2a46a58b
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2001-09-09 01:53:14 +0000

    oops small bug

--- code/task_fishing.cc
+++ code/task_fishing.cc
@@ -29,7 +29,9 @@ void TBeing::doFish(const char *direction){
 
   sendTo("You start fishing.\n\r");
 
-  if(getCond(DRUNK) > 10 && !::number(0,10)){
+  if(getCond(DRUNK) > 10 && !::number(0,10) &&
+     inRoom() < 31800 && inRoom() > 31899){
+
     sendTo("All of this drunken fishing has caused you to pass out.\n\r");
     sendTo("Strange things begin running through your mind...\n\r");
 
