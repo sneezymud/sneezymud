5cd8a973530d655cc70a2e208698289814c6f26e
jesus <jesus@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2001-09-27 20:58:43 +0000

    *** empty log message ***

--- code/bwiz.cc
+++ code/bwiz.cc
@@ -104,7 +104,7 @@ void recvTextHandler(const char *str)
     och = d->original ? d->original : d->character;
     if (!och)
       continue;
-    if (och->hasWizPower(POWER_WIZNET) || !d->isEditing()) {
+    if (och->hasWizPower(POWER_WIZNET)) {
       och->sendTo(buf);
     } else {
       // do nothing
