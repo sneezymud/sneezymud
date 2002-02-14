2476ee4929c7c995d14a945c22e8c417702fdcf7
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2002-02-14 00:15:28 +0000

    *** empty log message ***

--- code/socket.cc
+++ code/socket.cc
@@ -539,7 +539,13 @@ int TSocket::gameLoop()
 	      TBeing *tb;
 	      if(!::number(0,4) && (tb=dynamic_cast<TBeing *>(t))){
 		tb->sendTo(COLOR_BASIC, "<r>The large amount of smoke in the room causes you to choke and cough!<1>\n\r");
-		tb->reconcileDamage(tb, ::number(3,11), DAMAGE_SUFFOCATION);
+		rc=tb->reconcileDamage(tb, ::number(3,11), DAMAGE_SUFFOCATION);
+
+		if (IS_SET_DELETE(rc, DELETE_VICT)) {
+		  delete tb;
+		  continue;
+		}
+
 	      }
 	    }
 	  }
