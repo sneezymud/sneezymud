cd38f03350929dcb21c7845985de6c439775061b
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2002-03-04 17:54:57 +0000

    adding TTiming

---
+++ code/timing.cc
@@ -0,0 +1,31 @@
+#include "timing.h"
+
+double TTiming::to_secs(struct timeval tv){
+  return tv.tv_sec + ((tv.tv_usec)/1000000.0);
+}
+
+void TTiming::start(){
+  gettimeofday(&tv_start, &tz);
+}
+
+void TTiming::end(){
+  gettimeofday(&tv_end, &tz);
+}
+
+double TTiming::getStart(){
+  return to_secs(tv_start);
+}
+
+double TTiming::getEnd(){
+  return to_secs(tv_end);
+}
+
+double TTiming::getElapsed(){
+  return (to_secs(tv_end)-to_secs(tv_start));
+}
+
+TTiming::TTiming(){
+  tv_start.tv_sec=tv_start.tv_usec=0;
+  tv_end.tv_sec=tv_end.tv_usec=0;
+}
+
---
+++ code/timing.h
@@ -0,0 +1,24 @@
+#ifndef __TIMING_H
+#define __TIMING_H
+
+#include<sys/time.h>
+
+
+class TTiming {
+  struct timeval tv_start, tv_end;
+  struct timezone tz;
+  double to_secs(struct timeval);
+
+ public:
+  void start();
+  void end();
+  double getStart();
+  double getEnd();
+  double getElapsed();
+
+  TTiming();
+};
+
+
+
+#endif __TIMING_H
