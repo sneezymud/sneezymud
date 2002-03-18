36b5bbfdb3c89d8c7a39885b985baf707319441f
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2002-03-18 18:32:18 +0000

    fixed prototype and strung flags on added objects

--- code/sqladdobjs.cc
+++ code/sqladdobjs.cc
@@ -8,7 +8,8 @@ int main(int argc, char **argv)
   TDatabase db_beta("sneezybeta");
   string immortal;
   vector<int>vnums;
-
+  int action_flag;
+  
   if((argc-1) < 2){
     printf("Usage: %s <immortal> <obj list>\n", argv[0]);;
     exit(0);
@@ -28,8 +29,20 @@ int main(int argc, char **argv)
     
     if(db_immo.fetchRow()){
       printf("Adding %i\n", vnums[t]);
+      
+      // fix strung and prototype bits
+      action_flag=atoi_safe(db_immo.getColumn(6));
+      if(action_flag & (1<<2)){
+	action_flag=action_flag - (1<<2);
+      }
+      
+      if(action_flag & (1<<4)){
+	action_flag=action_flag - (1<<4);
+      }
+
+
       db_beta.query("delete from obj where vnum=%i", vnums[t]);
-      db_beta.query("insert into obj values(%s, '%s', '%s', '%s', '%s', %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", db_immo.getColumn(0), db_immo.getColumn(1), db_immo.getColumn(2), db_immo.getColumn(3), db_immo.getColumn(4), db_immo.getColumn(5), db_immo.getColumn(6), db_immo.getColumn(7), db_immo.getColumn(8), db_immo.getColumn(9), db_immo.getColumn(10), db_immo.getColumn(11), db_immo.getColumn(12), db_immo.getColumn(13), db_immo.getColumn(14), db_immo.getColumn(15), db_immo.getColumn(16), db_immo.getColumn(17), db_immo.getColumn(18), db_immo.getColumn(19), db_immo.getColumn(20), db_immo.getColumn(21));
+      db_beta.query("insert into obj values(%s, '%s', '%s', '%s', '%s', %s, %i, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", db_immo.getColumn(0), db_immo.getColumn(1), db_immo.getColumn(2), db_immo.getColumn(3), db_immo.getColumn(4), db_immo.getColumn(5), action_flag, db_immo.getColumn(7), db_immo.getColumn(8), db_immo.getColumn(9), db_immo.getColumn(10), db_immo.getColumn(11), db_immo.getColumn(12), db_immo.getColumn(13), db_immo.getColumn(14), db_immo.getColumn(15), db_immo.getColumn(16), db_immo.getColumn(17), db_immo.getColumn(18), db_immo.getColumn(19), db_immo.getColumn(20), db_immo.getColumn(21));
     } else {
       printf("Not found: %i\n", vnums[t]);
     }
@@ -42,7 +55,7 @@ int main(int argc, char **argv)
       db_beta.query("insert into objaffect values(%s, %s, %s, %s)", db_immo.getColumn(0), db_immo.getColumn(1), db_immo.getColumn(2), db_immo.getColumn(3));
     }      
 
-
+    
     //// obj extra
     db_immo.query("select vnum, name, description from objextra where owner='%s' and vnum=%i", immortal.c_str(), vnums[t]);
 
@@ -50,7 +63,10 @@ int main(int argc, char **argv)
       db_beta.query("delete from objextra where vnum=%i", vnums[t]);
       db_beta.query("insert into objextra values(%s, '%s', '%s')", db_immo.getColumn(0), db_immo.getColumn(1), db_immo.getColumn(2));
     }
+
   }
 
 }
+
+
 
