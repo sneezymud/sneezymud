05cf5b89c4d4fd2e7a5f9931099e85f9b9ff8b4d
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2002-10-17 23:23:22 +0000

    moved main() from comm.cc to main.cc pulled everything except main.cc into libmisc.so main() is now the only thing in the executable, everything else is in libraries

---
+++ code/main.cc
@@ -0,0 +1,120 @@
+//////////////////////////////////////////////////////////////////////////
+//
+// SneezyMUD - All rights reserved, SneezyMUD Coding Team
+//
+//////////////////////////////////////////////////////////////////////////
+
+#include "stdsneezy.h"
+#include "socket.h"
+
+extern "C" {
+#include <unistd.h>
+}
+
+extern int run_the_game();
+
+
+#ifndef LOWTOOLS
+
+int main(int argc, char *argv[])
+{
+#if 0
+  printf("Checking new/delete\n");
+  char *s = mud_str_dup("test string");
+  delete s;
+  printf("Checking unused.\n");
+  int x = 5;
+  int j;
+  printf("done\n");
+  exit(0);
+#endif
+  int a, pos = 1;
+  char dir[256];
+
+  gamePort = PROD_GAMEPORT;   // set as default
+  strcpy(dir, DFLT_DIR);
+
+  while ((pos < argc) && (*(argv[pos]) == '-')) {
+    switch (*(argv[pos] + 1)) {
+      case 'd':
+	if (*(argv[pos] + 2))
+	  strcpy(dir, argv[pos] + 2);
+	else if (++pos < argc)
+	  strcpy(dir, argv[pos]);
+	else {
+	  vlogf(LOG_MISC, "Directory arg expected after option -d.");
+	  exit(0);
+	}
+	break;
+      case 's':
+	noSpecials = 1;
+	vlogf(LOG_MISC, "Suppressing assignment of special routines.");
+	break;
+      default:
+	vlogf(LOG_MISC, "Unknown option -% in argument string.", *(argv[pos] + 1));
+	break;
+    }
+    pos++;
+  }
+
+  if (pos < argc) {
+    if (!isdigit(*argv[pos])) {
+      vlogf(LOG_MISC, "Usage: %s [-s] [-d pathname] [ port # ]\n", argv[0]);
+      exit(0);
+    } else if ((gamePort = atoi_safe(argv[pos])) <= 1024) {
+      printf("Illegal port #\n");
+      exit(0);
+    }
+  }
+  Uptime = time(0);
+
+  vlogf(LOG_MISC, "Running %s on port %d.", MUD_NAME, gamePort);
+
+  if (chdir(dir) < 0) {
+    perror("chdir");
+    exit(0);
+  }
+  vlogf(LOG_MISC, "Using %s as data directory.", dir);
+
+  srandom(time(0));
+
+  WizLock = false;
+
+  if (gamePort == BETA_GAMEPORT) {
+    vlogf(LOG_MISC, "Running on beta test site.  Wizlocking by default.");
+    WizLock = TRUE;
+  }
+
+  vlogf(LOG_MISC, "Blanking denied hosts.");
+  for (a = 0; a < MAX_BAN_HOSTS; a++) {
+    strcpy(hostLogList[a], "");
+    strcpy(hostlist[a], "");
+  }
+  numberhosts = 0;
+  numberLogHosts = 0;
+
+#if 0
+  extern void convert_all_pfiles();
+  convert_all_pfiles();
+#endif
+#if 0
+  extern void convert_all_rentfiles();
+  convert_all_rentfiles();
+#endif
+#if 0
+  // graceful, but too bad its not informative about the exception
+  try {
+    run_the_game();
+  } catch (...) {
+    mud_assert(0, "Caught an exception");
+  }
+#else
+  run_the_game();
+#endif
+
+  generic_cleanup();
+
+  return (0);
+}
+
+#endif
