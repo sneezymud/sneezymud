4296e303996dd1141f0c1cef8f3341f3dd7464df
dash <dash@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2003-05-04 01:20:20 +0000

    fixed the get race bug

--- code/race.cc
+++ code/race.cc
@@ -809,8 +809,8 @@ race_t TBeing::getRaceIndex(const char *ccName) const
 
 race_t TBeing::getRace() const
 {
-  if (is_disguised == TRUE && disguise_race) 
-    return disguise_race->getRace();
+  //  if (is_disguised == TRUE && disguise_race) 
+  //  return disguise_race->getRace();
   return race->getRace();
 }
 
