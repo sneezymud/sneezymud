5bc67f31e4dd2357c98986e60b7ae8ec44872368
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2003-05-22 19:15:32 +0000

    fixed memory leak with fortune cookies

--- code/spec_objs.cc
+++ code/spec_objs.cc
@@ -7218,12 +7218,12 @@ int fortuneCookie(TBeing *ch, cmdTypeT cmd, const char *, TObj *o, TObj *)
     ch->unequip(o->eq_pos);
   --(*o);  // remove from owner
   *cookie = *o;  // TObj assignment, copy values
-  //  delete o; // remove old object
   cookie->assignFourValues(3,0,0,0); // 3 food value, no flags
   cookie->swapToStrung();
 
   *ch += *cookie;
   *ch += *fortune;
+  *ch += *o; // have to put it back to return delete properly
 
   ssprintf(buf, "$n tears open $p and pulls out %s.", fortune->shortDescr);
   act(buf,TRUE,ch,cookie,NULL, TO_ROOM,NULL);
