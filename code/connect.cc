9b3386fdd742f8fa427c264aae13ff89360d3f5a
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2003-05-23 16:30:51 +0000

    Updated the login message to reflect yet another year gone by in sneezy history

--- code/connect.cc
+++ code/connect.cc
@@ -4933,7 +4933,7 @@ int Descriptor::sendLogin(const char *arg)
       sprintf(buf2 + strlen(buf2), "\n\r\n\rWelcome to %s:\n\r%s :\n\r", MUD_NAME_VERS, buf);
       fclose(fp);
     }
-    sprintf(buf2 + strlen(buf2), "Celebrating TEN years of quality mudding (est. 5-1-1992)\n\r\n\r");
+    sprintf(buf2 + strlen(buf2), "Celebrating ELEVEN years of quality mudding (est. 5-1-1992)\n\r\n\r");
     sprintf(buf2 + strlen(buf2), "Please type NEW (case sensitive) for a new account, or ? for help.\n\r\n\r");
     sprintf(buf2 + strlen(buf2), "\n\rLogin: ");
     output.putInQ(buf2);
