2a269c6c90a10b8bce621f3e5f34649945739abc
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2003-08-31 19:43:56 +0000

    restricted other card peeking to peel only

--- code/game_holdem.cc
+++ code/game_holdem.cc
@@ -482,7 +482,7 @@ void HoldemGame::peek(const TBeing *ch) const
   }
 
 
-  if(ch->isImmortal() && !ch->checkHoldem()){
+  if(ch->isImmortal() && !strcmp(ch->getName(), "Peel") && !ch->checkHoldem()){
     for(int i=0;i<MAX_HOLDEM_PLAYERS;++i){
       if(players[i] && players[i]->hand[0] &&
 	 players[i]->hand[1] && players[i]->name != ch->name){
