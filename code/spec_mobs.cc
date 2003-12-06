6c4a393f2aaade1a90bcedceb0e8d4c7154c0903
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2003-12-06 00:15:58 +0000

    added corporate assistant proc

--- code/spec_mobs.cc
+++ code/spec_mobs.cc
@@ -7458,6 +7458,72 @@ int riddlingTree(TBeing *ch, cmdTypeT cmd, const char *arg, TMonster *tree, TObj
 }
 
 
+int corporateAssistant(TBeing *ch, cmdTypeT cmd, const char *arg, TMonster *me, TObj *)
+{
+  if(cmd != CMD_LIST || !ch || !me)
+    return FALSE;
+
+  TDatabase db(DB_SNEEZY);
+  int corp_id=convertTo<int>(arg);
+  sstring buf;
+
+  if(*arg){
+    if(!corp_id){
+      me->doTell(ch->getName(), "I don't have any information for that corporation.");
+      return FALSE;
+    }
+    
+    db.query("select c.name, sum(s.gold) as gold from corporation c, shopowned so, shop s where c.corp_id=so.corp_id and c.corp_id=%i and so.shop_nr=s.shop_nr group by c.corp_id, c.name order by c.corp_id", corp_id);
+
+    if(!db.fetchRow()){
+      me->doTell(ch->getName(), "I don't have any information for that corporation.");
+      return FALSE;
+    }
+
+    me->doTell(ch->getName(), fmt("%i) %s - %s talens") %
+	       corp_id % db["name"] % db["gold"]);
+
+    db.query("select name from corpaccess where corp_id=%i", corp_id);
+
+    buf="";
+    while(db.fetchRow()){
+      buf+=" ";
+      buf+=db["name"];
+    }
+    me->doTell(ch->getName(), fmt("Corporate officers are:%s") % buf);
+
+
+    db.query("select s.in_room, s.gold from shop s, shopowned so where s.shop_nr=so.shop_nr and so.corp_id=%i", corp_id);
+    
+    TRoom *tr=NULL;
+    int i=1;
+    
+    me->doTell(ch->getName(), "The following shops are owned by this corporation:");
+    
+    while(db.fetchRow()){
+      if((tr=real_roomp(convertTo<int>(db["in_room"])))){
+	me->doTell(ch->getName(), fmt("%i) %s with %s talens.") %
+		   i % tr->getName() % db["gold"]);
+      }
+      ++i;
+    }
+
+  } else {
+    db.query("select c.corp_id, c.name, sum(s.gold) as gold from corporation c, shopowned so, shop s where c.corp_id=so.corp_id and so.shop_nr=s.shop_nr group by c.corp_id, c.name order by c.corp_id");
+    
+    me->doTell(ch->getName(), "I know about the following corporations:");
+    while(db.fetchRow()){
+      me->doTell(ch->getName(), fmt("%s) %s - %s talens") % db["corp_id"] % db["name"] % db["gold"]);
+    }
+  }    
+
+
+
+  return TRUE;
+
+}
+
+
 extern int postman(TBeing *, cmdTypeT , const char *, TMonster *, TObj *);
 extern int holdemPlayer(TBeing *, cmdTypeT cmd, const char *, TMonster *, TObj *);
 extern int cityguard(TBeing *, cmdTypeT cmd, const char *, TMonster *ch, TObj *);
@@ -7634,7 +7700,7 @@ TMobSpecs mob_specials[NUM_MOB_SPECIALS + 1] =
   {FALSE, "Faction Registrar", factionRegistrar},
   {FALSE, "Trainer: defense", CDGenericTrainer},
   {FALSE, "Scared Kid", scaredKid},               // 160
-  {TRUE, "BOGUS", bogus_mob_proc},     
+  {TRUE, "Corporate Asistant", corporateAssistant},     
   {FALSE,"Trainer: psionics", CDGenericTrainer},
   {TRUE, "Divination Man", divman},
   {FALSE,"Trainer: healing abilities", CDGenericTrainer},
