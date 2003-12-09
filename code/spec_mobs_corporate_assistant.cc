f56d5bc2c753a3b2cd75ddbfdb875b0409f1bef0
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2003-12-09 01:18:51 +0000

    more prettification added assets to main listing

--- code/spec_mobs_corporate_assistant.cc
+++ code/spec_mobs_corporate_assistant.cc
@@ -1,6 +1,36 @@
 #include "stdsneezy.h"
 #include "database.h"
 
+
+int getAssets(int corp_id)
+{
+  int value=0, keepernum=0;
+  TDatabase db(DB_SNEEZY);
+  TObj *o=NULL;
+  TRoom *room;
+  TMonster *keeper;
+
+  db.query("select in_room, keeper from shop where shop_nr in (select shop_nr from shopowned where corp_id=%i)", corp_id);
+  
+  while(db.fetchRow()){
+    room=real_roomp(convertTo<int>(db["in_room"]));
+    keepernum=convertTo<int>(db["keeper"]);
+
+    for(TThing *tt=room->getStuff();tt;tt=tt->nextThing){
+      if((keeper=dynamic_cast<TMonster *>(tt)) &&
+	 keeper->mobVnum() == keepernum){
+	for(TThing *t=keeper->getStuff();t;t=t->nextThing){
+	  o=dynamic_cast<TObj *>(t);
+	  value+=o->obj_flags.cost;
+	}
+        break;
+      }
+    }
+  }
+
+  return value;
+}
+
 sstring talenDisplay(int talens)
 {
   float t;
@@ -8,7 +38,7 @@ sstring talenDisplay(int talens)
   if(talens>1000000){
     t=(int)(talens/100000);
     return fmt("%.1fM") % (t/10.0);
-  } else if(talens > 1000){
+  } else if(talens > 10000){
     t=(int)(talens/1000);
     return fmt("%ik") % (int)t;
   }
@@ -20,28 +50,33 @@ sstring talenDisplay(int talens)
 void corpListing(TBeing *ch, TMonster *me)
 {
   TDatabase db(DB_SNEEZY);
+  int corp_id=0;
   
   db.query("select c.corp_id, c.name, sum(s.gold) as gold from corporation c, shopowned so, shop s where c.corp_id=so.corp_id and so.shop_nr=s.shop_nr group by c.corp_id, c.name order by gold desc");
   
   me->doTell(ch->getName(), "I know about the following corporations:");
   while(db.fetchRow()){
-    me->doTell(ch->getName(), fmt("%-2s| %s - %s talens") % db["corp_id"] % db["name"] % talenDisplay(convertTo<int>(db["gold"])));
+    corp_id=convertTo<int>(db["corp_id"]);
+
+    me->doTell(ch->getName(), fmt("%-2i| %s - %s talens, %s in assets") % 
+	       corp_id % db["name"] % 
+	       talenDisplay(convertTo<int>(db["gold"])) % 
+	       talenDisplay(getAssets(corp_id)));
   }
 }
 
 void corpSummary(TBeing *ch, TMonster *me, int corp_id)
 {
   TDatabase db(DB_SNEEZY);
-  int value=0, gold=0;
-  TObj *o=NULL;
-  TMonster *keeper;
-  
+  int value=0, gold=0, shopcount=0;
+  sstring buf;
+
   if(!corp_id){
     me->doTell(ch->getName(), "I don't have any information for that corporation.");
     return;
   }
   
-  db.query("select c.name, sum(s.gold) as gold from corporation c, shopowned so, shop s where c.corp_id=so.corp_id and c.corp_id=%i and so.shop_nr=s.shop_nr group by c.corp_id, c.name order by c.corp_id", corp_id);
+  db.query("select c.name, sum(s.gold) as gold, count(s.shop_nr) as shops from corporation c, shopowned so, shop s where c.corp_id=so.corp_id and c.corp_id=%i and so.shop_nr=s.shop_nr group by c.corp_id, c.name order by c.corp_id", corp_id);
   
   if(!db.fetchRow()){
     me->doTell(ch->getName(), "I don't have any information for that corporation.");
@@ -52,40 +87,23 @@ void corpSummary(TBeing *ch, TMonster *me, int corp_id)
 	     corp_id % db["name"]);
 
   gold=convertTo<int>(db["gold"]);
+  value=getAssets(corp_id);
+  shopcount=convertTo<int>(db["shops"]);
 
-
-
-  vector <int> keepers;
-  db.query("select keeper from shop where shop_nr in (select shop_nr from shopowned where corp_id=%i)", corp_id);
-  while(db.fetchRow()){
-    keepers.push_back(convertTo<int>(db["keeper"]));
-  }
-
-
-  for(TBeing *tb=character_list;tb;tb=tb->next){
-    for(unsigned int k=0;k<keepers.size();++k){
-      if((keeper=dynamic_cast<TMonster *>(tb)) &&
-	 keeper->mobVnum() == keepers[k]){
-	for(TThing *tt=keeper->getStuff();tt;tt=tt->nextThing){
-	  o=dynamic_cast<TObj *>(tt);
-	  value+=o->obj_flags.cost;
-	}
-      }
-    }
-  }
-  
-  me->doTell(ch->getName(), fmt("Talens:      %10i") % gold);
-  me->doTell(ch->getName(), fmt("Assets:      %10i") % value);
-  me->doTell(ch->getName(), fmt("Shops:       %10i") % keepers.size());
-
-  me->doTell(ch->getName(), fmt("Total value: %10i") %
-	     (gold+value+(keepers.size() * 1000000)));
+  me->doTell(ch->getName(), fmt("Talens:      %12s") % 
+	     (fmt("%i") % gold).comify());
+  me->doTell(ch->getName(), fmt("Assets:      %12s") % 
+	     (fmt("%i") % value).comify());
+  me->doTell(ch->getName(), fmt("Shops (x1M): %12s") % 
+	     (fmt("%i") % (shopcount*1000000)).comify());
+  me->doTell(ch->getName(), fmt("Total value: %12s") %
+	     (fmt("%i") % (gold+value+(shopcount * 1000000))).comify());
 
 
   // officers
   db.query("select name from corpaccess where corp_id=%i", corp_id);
   
-  sstring buf="";
+  buf="";
   while(db.fetchRow()){
     buf+=" ";
     buf+=db["name"];
@@ -104,8 +122,9 @@ void corpSummary(TBeing *ch, TMonster *me, int corp_id)
   
   while(db.fetchRow()){
     if((tr=real_roomp(convertTo<int>(db["in_room"])))){
+      gold=convertTo<int>(db["gold"]);
       me->doTell(ch->getName(), fmt("%-3s| %s with %s talens.") %
-		 db["shop_nr"] % tr->getName() % db["gold"]);
+		 db["shop_nr"] % tr->getName() % talenDisplay(gold));
     }
   }
 }
@@ -128,8 +147,11 @@ int corporateAssistant(TBeing *ch, cmdTypeT cmd, const char *arg, TMonster *me, 
 
   // list details of a specific corporation
   if(*arg){
-    tmp=convertTo<int>(arg);
-    corpSummary(ch, me, tmp);
+    if((tmp=convertTo<int>(arg))){
+      corpSummary(ch, me, tmp);
+    }
+
+
     return true;
   }    
 
