a56fe98e2bf81511e1e61447b2601fe361f5cdd3
maror <maror@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2004-01-14 04:46:15 +0000

    - turned off sweeps

--- code/mob_loader.cc
+++ code/mob_loader.cc
@@ -1156,7 +1156,7 @@ void TMonster::genericMobLoader(TOpenContainer **bag)
   *bag = NULL;
 
   //sneezy sweeps
-#if 1
+#if 0
   if (!::number(0,29) && wealth > 0)
   {
     TObj *tile = read_object(29301, VIRTUAL);
