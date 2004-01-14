1f0a34e3e51157b245b244598e5946f86f561684
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2004-01-14 21:43:20 +0000

    turned psionics damaged way down

--- code/skill_dam.cc
+++ code/skill_dam.cc
@@ -486,7 +486,7 @@ int TBeing::getSkillDam(const TBeing *victim, spellNumT skill, int level, int ad
     case SKILL_MIND_THRUST:
     case SKILL_PSYCHIC_CRUSH:
     case SKILL_KINETIC_WAVE:
-      dam = genericDam(victim, skill, DISC_PSIONICS, level, adv_learn, 0.639, REDUCE_YES, !isPc(), TRIM_NO);
+      dam = genericDam(victim, skill, DISC_PSIONICS, level, adv_learn, 0.200, REDUCE_YES, !isPc(), TRIM_NO);
       break;
     case SPELL_SKY_SPIRIT:
     case SPELL_EARTHMAW:
