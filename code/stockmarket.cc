c4c152dba4da75771d662bd0420a45209cee8473
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2004-04-19 18:57:40 +0000

    removed historic price adjustment for splits added a market average to board

--- code/stockmarket.cc
+++ code/stockmarket.cc
@@ -17,9 +17,8 @@ void stockSplit(sstring ticker, float pricechange)
 
   db.query("update stockinfo set shares=shares*2, price=(price/2)+%f where ticker='%s'", pricechange, ticker.c_str());
   
-  db.query("update stockhistory set price=price/2 where ticker='%s'",
-	   ticker.c_str());
-  
+  //  db.query("update stockhistory set price=price/2 where ticker='%s'",
+  //	   ticker.c_str());
 
   //  db.query("insert into stockhistory select max(n), ticker, max(price) from stockhistory where ticker='%s' group by ticker", ticker.c_str());
   //  db.query("insert into stockhistory select max(n), ticker, min(price) from stockhistory where ticker='%s' group by ticker", ticker.c_str());
@@ -29,11 +28,11 @@ void stockReverseSplit(sstring ticker, float pricechange)
 {
   TDatabase db(DB_SNEEZY);
 
-  db.query("update stockinfo set shares=shares/2, price=(price*2)+%f where ticker='%s'", pricechange, ticker.c_str());
+  db.query("update stockinfo set shares=shares/10, price=(price*2)+%f where ticker='%s'", pricechange, ticker.c_str());
 
-  db.query("update stockhistory set price=price*2 where ticker='%s'",
-	   ticker.c_str());
-  
+  //  db.query("update stockhistory set price=price*10 where ticker='%s'",
+	   //	   ticker.c_str());
+    
   //  db.query("insert into stockhistory select max(n), ticker, max(price) from stockhistory where ticker='%s' group by ticker", ticker.c_str());
   //  db.query("insert into stockhistory select max(n), ticker, min(price) from stockhistory where ticker='%s' group by ticker", ticker.c_str());
 }
@@ -73,6 +72,8 @@ int stockBoard(TBeing *ch, cmdTypeT cmd, const char *arg, TObj *o1, TObj *o2)
   TThing *o;
   TObj *to;
   TDatabase db(DB_SNEEZY);
+  float price, pricediff;
+  int shares;
 
   if(cmd != CMD_LOOK)
     return FALSE;
@@ -91,14 +92,26 @@ int stockBoard(TBeing *ch, cmdTypeT cmd, const char *arg, TObj *o1, TObj *o2)
 
   ch->sendTo("You examine the board:\n\r");
   ch->sendTo("------------------------------------------------------------\n\r");
+  db.query("select sum(sh.price*si.shares)/sum(si.shares) as dayprice, sum(si.price*si.shares)/sum(si.shares) as price from stockinfo si, stockhistory sh, (select ticker, max(n) as n from stockhistory group by ticker) sn where sh.n=sn.n and si.ticker=sh.ticker");
+  
+  if(db.fetchRow()){
+    price=convertTo<float>(db["price"]);
+    pricediff=price-convertTo<float>(db["dayprice"]);
+    
+    ch->sendTo(COLOR_BASIC, fmt("Market: <Y>%.2f  %s%+.2f<1>\n\r") %
+	       price % (pricediff>0?"<G>":"<R>") %
+	       (convertTo<float>(db["dayprice"]) - price));
+  }
+			
+
+
+  ch->sendTo("------------------------------------------------------------\n\r");
   ch->sendTo("Ticker   Bid    Ask  Change  Market Cap                                \n\r");
   ch->sendTo("------------------------------------------------------------\n\r");
   
   db.query("select distinct si.ticker, si.price, sh.price as dayprice, si.shares from stockinfo si, stockhistory sh, (select ticker, max(n) as n from stockhistory group by ticker) sn where sh.n=sn.n and si.ticker=sh.ticker");
 
 
-  float price, pricediff;
-  int shares;
   while(db.fetchRow()){
     price=convertTo<float>(db["price"]);
     shares=convertTo<int>(db["shares"]);
@@ -108,9 +121,8 @@ int stockBoard(TBeing *ch, cmdTypeT cmd, const char *arg, TObj *o1, TObj *o2)
 	       db["ticker"] % (price*0.97) % (price * 1.03) % 
 	       (pricediff>0?"<G>":"<R>") % pricediff %
 	       talenDisplay((int)(price*(float)shares)));
-    
-    
   }
+
 
   ch->sendTo("------------------------------------------------------------\n\r");
   ch->sendTo("Price charts can be seen at: http://sneezy.saw.net/peel/stocks/\n\r");
