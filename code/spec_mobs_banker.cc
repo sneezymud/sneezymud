a563c1f20817e5f2ddc92b6f609e7fb8504a4565
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2004-05-27 21:19:55 +0000

    added "list"

--- code/spec_mobs_banker.cc
+++ code/spec_mobs_banker.cc
@@ -1,13 +1,14 @@
 #include "stdsneezy.h"
 #include "database.h"
 #include "shop.h"
+#include "shopowned.h"
 
 int banker(TBeing *ch, cmdTypeT cmd, const char *arg, TMonster *myself, TObj *)
 {
   int shop_nr, money, bankmoney=-1;
   TDatabase db(DB_SNEEZY);
 
-  if((cmd!=CMD_WITHDRAW && cmd!=CMD_DEPOSIT && cmd!=CMD_BUY &&
+  if((cmd!=CMD_WITHDRAW && cmd!=CMD_DEPOSIT && cmd!=CMD_BUY && cmd!=CMD_LIST &&
      cmd!=CMD_BALANCE && cmd!=CMD_WHISPER) || !ch || !myself)
     return FALSE;
 
@@ -16,6 +17,33 @@ int banker(TBeing *ch, cmdTypeT cmd, const char *arg, TMonster *myself, TObj *)
   
   if(cmd==CMD_WHISPER)
     return shopWhisper(ch, myself, shop_nr, arg);
+
+  if(cmd==CMD_LIST){
+    TShopOwned tso(shop_nr, myself, ch);
+
+    if(!tso.hasAccess(SHOPACCESS_LOGS)){
+      myself->doTell(ch->getName(), "Sorry, you don't have access to do that.");
+      return FALSE;
+    }
+    
+    db.query("select p.name, b.talens from player p, shopownedbank b where b.shop_nr=%i and b.player_id=p.id", shop_nr);
+    
+    while(db.fetchRow()){
+      myself->doTell(ch->getName(), fmt("%s - %s talens.") % db["name"] %
+		     talenDisplay(convertTo<int>(db["talens"])));
+    }
+
+    db.query("select count(*) as c, sum(talens) as t from shopowned bank");
+
+    if(db.fetchRow()){
+      myself->doTell(ch->getName(), fmt("%i accounts, %s talens.") %
+		     convertTo<int>(db["c"]) % 
+		     talenDisplay(convertTo<int>(db["t"])));
+    }
+
+    return TRUE;
+  }
+
 
   db.query("select talens from shopownedbank where player_id=%i and shop_nr=%i", ch->getPlayerID(), shop_nr);
   
@@ -54,12 +82,16 @@ int banker(TBeing *ch, cmdTypeT cmd, const char *arg, TMonster *myself, TObj *)
     if (money > bankmoney) {
       myself->doTell(ch->getName(), "You don't have enough in the bank for that!");
       return TRUE;
+    } else if(myself->getMoney() < money){
+      myself->doTell(ch->getName(), "The bank doesn't have your funds available right now!");
+      return TRUE;
     } else if (money <= 0) {
       myself->doTell(ch->getName(), "Go away, you bother me.");
       return TRUE;
     } else {
       myself->doTell(ch->getName(), "Thank you.");
       ch->addToMoney(money, GOLD_XFER);
+      myself->addToMoney(-money, GOLD_XFER);
       
       db.query("update shopownedbank set talens=talens-%i where player_id=%i and shop_nr=%i", money, ch->getPlayerID(), shop_nr);
 
@@ -90,6 +122,7 @@ int banker(TBeing *ch, cmdTypeT cmd, const char *arg, TMonster *myself, TObj *)
 
     myself->doTell(ch->getName(), "Thank you.");
     ch->addToMoney(-money, GOLD_XFER);
+    myself->addToMoney(money, GOLD_XFER);
     
     db.query("update shopownedbank set talens=talens+%i where player_id=%i and shop_nr=%i", money, ch->getPlayerID(), shop_nr);
 
