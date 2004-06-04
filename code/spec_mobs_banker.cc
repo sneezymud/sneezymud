f409131bae252d5e362735bdbde43860b3c1a3d1
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2004-06-04 20:09:03 +0000

    fixed query bug that was including all corporate accounts in "list" for all banks

--- code/spec_mobs_banker.cc
+++ code/spec_mobs_banker.cc
@@ -214,7 +214,7 @@ int banker(TBeing *ch, cmdTypeT cmd, const char *arg, TMonster *myself, TObj *)
       return FALSE;
     }
 
-    db.query("select p.name, b.talens from player p, shopownedbank b where b.shop_nr=%i and b.player_id=p.id union select c.name, b.talens from corporation c, shopownedcorpbank b where b.shop_nr=c.bank and b.corp_id=c.corp_id order by talens desc", shop_nr);
+    db.query("select p.name, b.talens from player p, shopownedbank b where b.shop_nr=%i and b.player_id=p.id union select c.name, b.talens from corporation c, shopownedcorpbank b where b.shop_nr=c.bank and b.shop_nr=%i and b.corp_id=c.corp_id order by talens desc", shop_nr, shop_nr);
 
     sstring buf;
     while(db.fetchRow()){
