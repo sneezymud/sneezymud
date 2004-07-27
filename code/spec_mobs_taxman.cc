795c3a27f97b13995092752eacf05716475cc8ba
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2004-07-27 00:09:17 +0000

    initial checkin

---
+++ code/spec_mobs_taxman.cc
@@ -0,0 +1,25 @@
+#include "stdsneezy.h"
+#include "shop.h"
+#include "database.h"
+
+int taxman(TBeing *ch, cmdTypeT cmd, const char *arg, TMonster *me, TObj *)
+{
+  TDatabase db(DB_SNEEZY);
+  int shop_nr=find_shop_nr(me->number);
+
+
+  if(cmd==CMD_WHISPER){
+    return shopWhisper(ch, me, shop_nr, arg);
+  } else if(cmd==CMD_LIST){
+    db.query("select s.shop_nr as shop_nr, r.name as name from shop s, shopownedtax st, room r where st.tax_nr=%i and st.shop_nr=s.shop_nr and r.vnum=s.in_room", shop_nr);
+    
+    while(db.fetchRow()){
+      me->doTell(ch->name, fmt("%-3s| <r>%s<1>") %
+		 db["shop_nr"] % db["name"]);
+    }
+    
+    return TRUE;
+  }
+
+  return FALSE;
+}
