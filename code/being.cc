d60fe812623e2b59f58e53f258912f9abd809c35
lapsos <lapsos@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2004-09-02 07:07:24 +0000

    Modified addToLifeforce to force a limit of 32000 limit to the lifeforce value.  This should prevent the loop-back-to-zero issue reported.

--- code/being.cc
+++ code/being.cc
@@ -918,6 +918,7 @@ void TBeing::addToLifeforce(int lifeforce)
 {
   points.lifeforce += lifeforce;
   points.lifeforce = max((short int) 0, points.lifeforce);
+  points.lifeforce = min(points.lifeforce, 32000);
 }
 
 bool TBeing::noLifeforce(int lifeforce) const
