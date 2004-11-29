2e57aeaebb1fff2492c9d87f735d18d91d620c17
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2004-11-29 23:54:33 +0000

    added imm and non-imm data

--- code/spec_mobs_property_clerk.cc
+++ code/spec_mobs_property_clerk.cc
@@ -22,14 +22,23 @@ int propertyClerk(TBeing *ch, cmdTypeT cmd, const char *argument, TMonster *me, 
       db.query("select p.id as id, p.name as name, p.owner as owner_id, pl.name as owner, p.key as key, o.short_desc as keyname, p.entrance as entrance_id, r.name as entrance from property p, player pl, obj o, room r where p.owner=pl.id and p.key=o.vnum and r.vnum=p.entrance and p.id=%s", arg.word(0).c_str());
       
       while(db.fetchRow()){
-	me->doTell(ch->getName(), fmt("%-2i| %s") % 
-		   convertTo<int>(db["id"]) % db["name"]);
-	me->doTell(ch->getName(), fmt("Owned by: %s (%i)") % 
-		   db["owner"] % convertTo<int>(db["owner_id"]));
-	me->doTell(ch->getName(), fmt("Key: %s (%i)") % 
-		   db["keyname"] % convertTo<int>(db["key"]));
-	me->doTell(ch->getName(), fmt("Entrance: %s (%i)") %
-		   db["entrance"] % convertTo<int>(db["entrance_id"]));
+	if(ch->isImmortal()){
+	  me->doTell(ch->getName(), fmt("%-2i| %s") % 
+		     convertTo<int>(db["id"]) % db["name"]);
+	  me->doTell(ch->getName(), fmt("Owned by: %s (%i)") % 
+		     db["owner"] % convertTo<int>(db["owner_id"]));
+	  me->doTell(ch->getName(), fmt("Key: %s (%i)") % 
+		     db["keyname"] % convertTo<int>(db["key"]));
+	  me->doTell(ch->getName(), fmt("Entrance: %s (%i)") %
+		     db["entrance"] % convertTo<int>(db["entrance_id"]));
+	} else {
+	  me->doTell(ch->getName(), fmt("%-2i| %s") % 
+		     convertTo<int>(db["id"]) % db["name"]);
+	  me->doTell(ch->getName(), fmt("Owned by: %s") % 
+		     db["owner"]);
+	  me->doTell(ch->getName(), fmt("Entrance: %s") %
+		     db["entrance"]);
+	}
       }
     }
     return true;
