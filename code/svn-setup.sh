b47dece5ebd6ec1cb8ba520601fa31508ed19ef4
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2005-01-17 21:32:23 +0000

    using svn now

---
+++ code/svn-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SVNROOT="file:///mud/source/trunk/code"
+cd $HOME
+svn checkout $SVNROOT
+ln -s /mud/build/lib code/lib
+echo "Your dev environment is now in $HOME/code."
+echo "An SVN tutorial is available: $HOME/code/svntutorial.txt"
