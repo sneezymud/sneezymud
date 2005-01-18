1a65b2dfb1f423020330e88cadcfabe2a757c2a6
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2005-01-18 18:11:49 +0000

    simple script for starting make

---
+++ code/build.sh
@@ -0,0 +1,36 @@
+#!/usr/local/bin/bash
+
+if [ "$1" = "" ]
+then
+  echo "Usage: $0 <start|stop>";
+elif [ "$1" = "start" ]
+then
+  pid=`pgrep -U peel -f "gmake"`
+  if [ "$pid" == "" ]
+  then
+    echo "Starting make, output to file.mak.";
+    gmake -k -j 5 -l 2 >& file.mak &
+  else
+    echo "You are already running make."
+  fi
+elif [ "$1" = "stop" ]
+then
+  pid=`pgrep -U peel -f "gmake"`
+  if [ "$pid" != "" ]
+  then
+    echo "Killing process $pid."
+    kill $pid
+  else
+    echo "Make process not found."
+  fi
+elif [ "$1" = "status" ]
+then
+  pid=`pgrep -U peel -f "gmake"`
+  if [ "$pid" != "" ]
+  then
+    echo "Make process found, pid $pid."
+  else
+    echo "Make process not found."
+  fi
+fi
+
