63e0e0af77cf325ddd5e4560ef02d03963b9bf6f
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2005-01-30 04:28:38 +0000

    mvlow script, new and improved

---
+++ code/mvlow.sh
@@ -0,0 +1,33 @@
+#!/usr/local/bin/bash
+
+BUILD_MUD_ROOT="/mud/build"
+PROD_MUD_ROOT="/mud/prod"
+
+umask 0002
+
+# copy the latest tiny files
+echo Copying tinyworld.mob
+cp $BUILD_MUD_ROOT/lib/tinyworld.mob $PROD_MUD_ROOT/lib/tinyworld.mob
+#echo Copying zonefiles directory
+#cp $BUILD_MUD_ROOT/lib/zonefiles/* $PROD_MUD_ROOT/lib/zonefiles
+#cd $PROD_MUD_ROOT/lib/zonefiles
+#cvs update
+
+# copy latest Crier Response File
+echo Copying town crier response file
+
+cp  $BUILD_MUD_ROOT/lib/mobdata/responses/crier.new $PROD_MUD_ROOT/lib/mobdata/responses/crier
+
+
+# note that we've removed indexes from sneezyq for speed, so pg_restore 
+# will complain about missing constraints
+echo ***** Note that "XXX_pkey does not exist" errors are ok - ignore them.
+for i in obj objextra objaffect \
+         shop shopproducing shopmaterial shoptype \
+         room roomexit roomextra; 
+do 
+  echo Copying $i table.
+  pg_dump -F c -t $i sneezybeta | pg_restore -c -d sneezyq
+done
+
+
