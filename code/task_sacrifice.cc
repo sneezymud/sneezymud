11f682271e406813aa939b7788c0f64154eb3f41
maror <maror@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2005-08-18 07:46:55 +0000

    changed update rate of sacrifice, possibly solving timing bug? also slowed it down slightly at Jesus' request

--- code/task_sacrifice.cc
+++ code/task_sacrifice.cc
@@ -120,7 +120,7 @@ int task_sacrifice(TBeing *ch, cmdTypeT cmd, const char *, int pulse, TRoom *, T
         }
       }
 
-      ch->task->calcNextUpdate(pulse, 2);
+      ch->task->calcNextUpdate(pulse, 2 * PULSE_MOBACT);
       totem->addToToolUses(-1);
       if (totem->getToolUses() <= 0) {
         act("Your $o has been confiscated by the loa! It must have been too weak.", 
