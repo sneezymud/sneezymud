3d5584b982eff282ebec12d7d7e704c136b4039b
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2005-08-24 19:08:49 +0000

    nevermind, I'm retarded.  no second make.

--- code/build.sh
+++ code/build.sh
@@ -11,11 +11,7 @@ then
   if [ "$pid" == "" ]
   then
     echo "Starting make, output to file.mak.";
-# the second gmake is to make sure the linking is done properly; sometimes
-# gmake gets confused with multiple processes.  The && makes sure that it
-# only runs if the first build completed successfully, so we don't rebuild
-# files that errored out
-    (gmake -k -j 2 -l 3 && gmake) >& file.mak &
+    gmake -k -j 2 -l 3 >& file.mak &
   else
     echo "You are already running make."
   fi
