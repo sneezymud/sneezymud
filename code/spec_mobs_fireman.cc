e6bf017f24b3df6710dd41a3bcf3644e1c4760ba
maror <maror@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2005-08-28 05:39:27 +0000

    increased distance firemen are willing to go to put out a fire

--- code/spec_mobs_fireman.cc
+++ code/spec_mobs_fireman.cc
@@ -46,7 +46,7 @@ static int findAFire(TMonster *myself)
 
   // findFire only works in grimhaven, so really we only need a limited range
   // if no fire is found, they'll just head back to the fire station in GH
-  path.setRange(50);
+  path.setRange(100);
 
   dir=path.findPath(myself->inRoom(), findFire());
 
