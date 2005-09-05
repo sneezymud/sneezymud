6d8604fe11507ea57848de302da4c2deec196040
Travis <Travis@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2005-09-05 15:20:52 +0000

    initial template

---
+++ pclient/src/pclient/Main.java
@@ -0,0 +1,30 @@
+/*
+ * Main.java
+ *
+ * Created on September 5, 2005, 5:19 PM
+ *
+ * To change this template, choose Tools | Options and locate the template under
+ * the Source Creation and Management node. Right-click the template and choose
+ * Open. You can then make changes to the template in the Source Editor.
+ */
+
+package pclient;
+
+/**
+ *
+ * @author peels
+ */
+public class Main {
+    
+    /** Creates a new instance of Main */
+    public Main() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
