70066a4f88017c49fe51c862a4677d140810de24
angus <angus@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2006-01-03 02:20:43 +0000

    Moved the location of the cgi to /low/objlog.cgi. Added some notes into the web form.

--- code/objlog.cgi.cc
+++ code/objlog.cgi.cc
@@ -11,7 +11,21 @@
 using namespace cgicc;
 
 
-int main(int argc, char **argv){
+void print_form()
+{
+  cout << "<form method=post action=\"/low/objlog.cgi\">" << endl;
+  cout << "Filter on Object Name: <input type=text name=name><br>" << endl;
+  cout << "Filter on Object VNum: <input type=text name=vnum><br>" << endl;
+  cout << "<input type=hidden name=start value=1" << ">";
+  cout << "<input type=submit><br>" << endl;
+  cout << "Notes:<br>" << endl;
+  cout << "1.  Leaving both fields blank will list all entries in the log.<br>" << endl;
+  cout << "2.  The object name field should use the % wildcard character.  Example: %shield%<br>" << endl;
+  cout << "3.  If both fields are filled in, the script ignores the VNum field.<br>" << endl;
+}
+
+int main(int argc, char **argv)
+{
   Cgicc cgi;
   TDatabase db(DB_SNEEZYPROD);
   // TDatabase db(DB_SNEEZYBETA);
@@ -25,30 +39,14 @@ int main(int argc, char **argv){
     cout << HTTPHTMLHeader() << endl;
     cout << html() << head(title("Object Load Logs")) << endl;
     cout << body() << endl;
-
-    cout << "<form method=post action=\"/angus/objlog.cgi\">" << endl;
-    cout << "Object Name: <input type=text name=name><br>" << endl;
-    cout << "Object VNum: <input type=text name=vnum><br>" << endl;
-    cout << "<input type=hidden name=start value=1" << ">";
-    cout << "<input type=submit><br>" << endl;
+    print_form();
     cout << body() << endl;
   } else {
     cout << HTTPHTMLHeader() << endl;
     cout << html() << head(title("Object Load Logs")) << endl;
     cout << body() << endl;
 
-    cout << "<form method=post action=\"/angus/objlog.cgi\">" << endl;
-    cout << "Object Name: <input type=text name=name><br>" << endl;
-    cout << "Object VNum: <input type=text name=vnum><br>" << endl;
-    cout << "<input type=hidden name=start value=1" << ">";
-    cout << "<input type=submit><br>" << endl;
-    if ((**vnum).empty() && (**name).empty()) {
-      db.query("select l.vnum, o.name, l.loadtime, l.objcount from objlog l, obj o where l.vnum = o.vnum order by l.loadtime");
-    } else if ((**vnum).empty()) {
-      db.query("select l.vnum, o.name, l.loadtime, l.objcount from objlog l, obj o where l.vnum = o.vnum and o.name like '%s' order by l.loadtime", (**name).c_str());
-    } else if ((**name).empty()) {
-      db.query("select l.vnum, o.name, l.loadtime, l.objcount from objlog l, obj o where l.vnum = o.vnum and l.vnum=%i order by l.loadtime", convertTo<int>(**vnum));
-    }
+    print_form();
     cout << "<table border=1>" << endl;
     cout << "  <tr>" << endl;
     cout << "    <th align center>VNum</th>" << endl;
@@ -57,6 +55,13 @@ int main(int argc, char **argv){
     cout << "    <th align center>Count</th>" << endl;
     cout << "  </tr>" << endl;
 
+    if ((**vnum).empty() && (**name).empty()) {
+      db.query("select l.vnum, o.name, l.loadtime, l.objcount from objlog l, obj o where l.vnum = o.vnum order by l.loadtime");
+    } else if ((**vnum).empty()) {
+      db.query("select l.vnum, o.name, l.loadtime, l.objcount from objlog l, obj o where l.vnum = o.vnum and o.name like '%s' order by l.loadtime", (**name).c_str());
+    } else if ((**name).empty()) {
+      db.query("select l.vnum, o.name, l.loadtime, l.objcount from objlog l, obj o where l.vnum = o.vnum and l.vnum=%i order by l.loadtime", convertTo<int>(**vnum));
+    }
     while(db.fetchRow()){
       cout << "  <tr valign=top>" << endl;
       cout << "    <td align=right>" << stripColorCodes(db["vnum"]) << "</td>" << endl;
