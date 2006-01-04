649f01a9df5c9a8cbb4bcee286b5506295cd19fa
angus <angus@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2006-01-04 04:52:22 +0000

    Added logic to filter by item types.

--- code/objlog.cgi.cc
+++ code/objlog.cgi.cc
@@ -10,33 +10,45 @@
 
 using namespace cgicc;
 
+TDatabase db(DB_SNEEZYPROD);
+// TDatabase db(DB_SNEEZYBETA);
+Cgicc cgi;
+
+form_iterator name = cgi.getElement("name");
+form_iterator object_type = cgi.getElement("object_type");
+form_iterator vnum = cgi.getElement("vnum");
+form_iterator start = cgi.getElement("start");
 
 void print_form()
 {
   cout << "<form method=post action=\"/low/objlog.cgi\">" << endl;
   cout << "Filter on Object Name: <input type=text name=name><br>" << endl;
+  cout << "Filter on Object Type: <select name=object_type>" << endl;
+
+  db.query("select substr(lower(i.name), 6) as object_type from itemtypes i order by i.name");
+  
+  cout << "<option value=ALL" << endl;
+  cout << "> ALL" << endl;
+  while(db.fetchRow()){
+    cout << "<option value=" << db["object_type"];
+    cout << "> " << db["object_type"] << endl;
+  }
+  cout << "</select><br>" << endl;
   cout << "Filter on Object VNum: <input type=text name=vnum><br>" << endl;
-  cout << "<input type=hidden name=start value=1" << ">";
+  cout << "<input type=hidden name=start value=1>" << endl;
   cout << "<input type=submit><br>" << endl;
   cout << "Notes:<br>" << endl;
   cout << "1.  Leaving both fields blank will list all entries in the log.<br>" << endl;
   cout << "2.  The object name field should use the % wildcard character.  Example: %shield%<br>" << endl;
-  cout << "3.  If both fields are filled in, the script ignores the VNum field.<br>" << endl;
+  cout << "3.  The script ignores the other filters if the VNum field is used.<br>" << endl;
 }
 
 int main(int argc, char **argv)
 {
-  Cgicc cgi;
   sstring my_query;
   unsigned int count = 0;
-  TDatabase db(DB_SNEEZYPROD);
-  // TDatabase db(DB_SNEEZYBETA);
 
   toggleInfo.loadToggles();
-
-  form_iterator name = cgi.getElement("name");
-  form_iterator vnum = cgi.getElement("vnum");
-  form_iterator start = cgi.getElement("start");
 
   if (start == cgi.getElements().end()) {
     cout << HTTPHTMLHeader() << endl;
@@ -60,16 +72,26 @@ int main(int argc, char **argv)
     cout << "  </tr>" << endl;
 
     // my_query = "SELECT l.vnum, i.name AS objtype, o.name, l.loadtime::TIMESTAMP(0), l.objcount FROM objlog l, obj o, itemtypes i WHERE l.vnum = o.vnum AND o.\"type\" = i.\"type\"";
-    my_query = "SELECT l.vnum, i.name AS objtype, o.name, l.loadtime::TIMESTAMP(0), l.objcount FROM objlog l LEFT OUTER JOIN obj o USING (vnum) LEFT OUTER JOIN itemtypes i USING (\"type\")";
+    my_query = "SELECT l.vnum, substr(lower(i.name), 6) AS objtype, o.name, l.loadtime::TIMESTAMP(0), l.objcount FROM objlog l LEFT OUTER JOIN obj o USING (vnum) LEFT OUTER JOIN itemtypes i USING (\"type\")";
     if ((**vnum).empty() && (**name).empty()) {
-      my_query += " ORDER BY l.loadtime";
-      db.query(my_query.c_str());
-    } else if ((**name).empty()) {
+      if ((**object_type) == "ALL") {
+        my_query += " ORDER BY l.loadtime";
+        db.query(my_query.c_str());
+      } else {
+        my_query += " WHERE lower(i.name) = lower('ITEM_' || '%s') ORDER BY l.loadtime";
+        db.query(my_query.c_str(), (**object_type).c_str());
+      }
+    } else if ((**vnum).empty()) {
+      if ((**object_type) == "ALL") {
+        my_query += " WHERE lower(o.name) LIKE lower('%s') ORDER BY l.loadtime";
+        db.query(my_query.c_str(), (**name).c_str());
+      } else {
+        my_query += " WHERE lower(o.name) LIKE lower('%s') AND lower(i.name) = lower('ITEM_' || '%s') ORDER BY l.loadtime";
+        db.query(my_query.c_str(), (**name).c_str(), (**object_type).c_str());
+      }
+    } else {
       my_query += " WHERE l.vnum=%i ORDER BY l.loadtime";
       db.query(my_query.c_str(), convertTo<int>(**vnum));
-    } else {
-      my_query += " WHERE lower(o.name) LIKE lower('%s') ORDER BY l.loadtime";
-      db.query(my_query.c_str(), (**name).c_str());
     }
     while(db.fetchRow()){
       count++;
