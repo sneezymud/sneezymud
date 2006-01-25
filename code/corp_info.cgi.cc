8995741c78bdbc59a1076d388fb5ad2cd8bded32
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2006-01-25 14:15:21 +0000

    added listing of corporations

--- code/corp_info.cgi.cc
+++ code/corp_info.cgi.cc
@@ -11,23 +11,58 @@
 
 using namespace cgicc;
 
-TDatabase db(DB_SNEEZYPROD);
-// TDatabase db(DB_SNEEZYBETA);
 Cgicc cgi;
 
+void sendCorporationsList();
+void sendCorporationInfo(int);
 
 int main(int argc, char **argv)
 {
-  sstring my_query;
-  multimap <int, sstring, std::greater<int> > m;
-
+  // trick the db code into using the prod database
+  gamePort = PROD_GAMEPORT;
   toggleInfo.loadToggles();
-  
+  form_iterator corp_id_form=cgi.getElement("corp_id");
+
   cout << HTTPHTMLHeader() << endl;
+
+  if(corp_id_form == cgi.getElements().end()){
+    sendCorporationsList();
+  } else {
+    sendCorporationInfo(convertTo<int>(**corp_id_form));
+  }
+}
+
+void sendCorporationInfo(int corp_id)
+{
+
+}
+
+void sendCorporationsList()
+{
+  vector <corp_list_data> corp_list;
+  multimap <int, int, std::greater<int> > m;
+  multimap <int, int, std::greater<int> >::iterator it;
+
   cout << html() << head(title("Corporations")) << endl;
   cout << body() << endl;
 
-  m=getCorpListingData();
+  corp_list=getCorpListingData();
+  
+  for(unsigned int i=0;i<corp_list.size();++i){
+    m.insert(pair<int,int>(corp_list[i].gold, i));
+  }
+
+  cout << "<table border=1>" << endl;
+  cout << "<tr><td>ID</td><td>Name</td><td>Gold</td></tr>" << endl;
+  for(it=m.begin();it!=m.end();++it){
+    cout << (fmt("<tr><td>%i</td><td><a href=\"corp_info.cgi?corp_id=%i\">%s</a></td><td>%s</td></tr>") %
+	     corp_list[(int)((*it).second)].corp_id %
+	     corp_list[(int)((*it).second)].corp_id %
+	     corp_list[(int)((*it).second)].name %
+	     talenDisplay((*it).first)).c_str();
+    cout << endl;
+  }
+  cout << "</table>";
 
   cout << body() << endl;
   cout << html() << endl;
