040a90e331f5ad7fc06b4dd3284029938d2da97f
angus <angus@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2006-03-01 04:50:19 +0000

    Added missing parameters to the syntax message for the gamestats command.

--- code/statistics.cc
+++ code/statistics.cc
@@ -312,7 +312,7 @@ void TBeing::doGamestats(const sstring &arg)
 
 
   if(buf.empty()){
-    sendTo("Syntax: gamestats <combat | equipment | level | trivia | statistics>\n\r");
+    sendTo("Syntax: gamestats <attributes | combat | equipment | levels | trivia | statistics>\n\r");
     return;
   } else if (is_abbrev(buf, "attributes")) {
     int temp_stat = getStat(STAT_CURRENT, STAT_STR);
@@ -506,7 +506,7 @@ void TBeing::doGamestats(const sstring &arg)
     systask->AddTask(this, SYSTEM_STATISTICS, "");
     return;
   }
-  sendTo("Syntax: gamestats <combat | equipment | level | trivia | statistics>\n\r");
+  sendTo("Syntax: gamestats <attributes | combat | equipment | levels | trivia | statistics>\n\r");
   return;
 }
 
