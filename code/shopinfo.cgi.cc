ef0d1fcbefb09d7714d03d446682f56284e685e7
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2006-03-09 13:21:36 +0000

    can now list shops and display basic info

--- code/shopinfo.cgi.cc
+++ code/shopinfo.cgi.cc
@@ -23,6 +23,7 @@ void sendLogin();
 void sendLoginCheck(Cgicc cgi, TSession);
 
 void sendShoplist(int);
+void sendShowShop(int, int);
 
 int main(int argc, char **argv)
 {
@@ -45,11 +46,11 @@ int main(int argc, char **argv)
     }
   } else {
     if(state_form == cgi.getElements().end() || **state_form == "main"){
-      //      sendShoplist(session.getAccountID());
-      sendShoplist(664);
+      sendShoplist(session.getAccountID());
       return 0;
-    } else if(**state_form == "listmessages"){
-      //      sendMessageList(cgi, session.getAccountID());
+    } else if(**state_form == "showshop"){
+      form_iterator shop_nr=cgi.getElement("shop_nr");
+      sendShowShop(session.getAccountID(), convertTo<int>(**shop_nr));
       return 0;
     } else if(**state_form == "logout"){
       session.logout();
@@ -78,6 +79,103 @@ int main(int argc, char **argv)
 
 }
 
+
+void sendShowShop(int account_id, int shop_nr)
+{
+  TDatabase db(DB_SNEEZY);
+
+  db.query("\
+select \
+  distinct so.shop_nr, so.no_such_item1, so.no_such_item2, so.do_not_buy, \
+  so.missing_cash1, so.missing_cash2, so.message_buy, so.message_sell, \
+  so.max_num, so.dividend, rtax.name as taxedby, s.gold, so.reserve_min, \
+  so.reserve_max, so.profit_buy, so.profit_sell, r.name as shopname, \
+  c.name as corpname, p.name as playername \
+from \
+  shop s left outer join shopownedtax sot on (sot.shop_nr=s.shop_nr) \
+         left outer join shop stax on (stax.shop_nr=sot.shop_nr) \
+         left outer join room rtax on (rtax.vnum=stax.in_room), \
+  corporation c, room r, \
+  shopowned so left outer join shopownedaccess soa on \
+  (so.shop_nr=soa.shop_nr), \
+  player p, corpaccess ca \
+where \
+  ca.corp_id=so.corp_id and p.account_id=%i and \
+  (lower(p.name)=lower(soa.name) or ca.player_id=p.id) and \
+  r.vnum=s.in_room and s.shop_nr=so.shop_nr and so.shop_nr=%i and \
+  c.corp_id=ca.corp_id \
+order by r.name",
+	   account_id, shop_nr);
+
+
+  cout << HTTPHTMLHeader() << endl;
+  cout << html() << head() << title("Shopinfo") << endl;
+  cout << head() << body() << endl;
+
+  if(!db.fetchRow()){
+    cout << "Shop not found or you don't have permission.";
+    cout << body() << endl;
+    cout << html() << endl; 
+    return;
+  }
+
+  
+  cout << "<table border=1>" << endl;
+  cout << "<tr><td>Shop #" << db["shop_nr"] << "</td><td>" << db["shopname"];
+  cout << "</td><td>" << db["corpname"] << "</td></tr>" << endl;
+
+  cout << "<tr><td>Cash</td><td>" << db["gold"] << "</td>";
+  cout << "<td>Taxed by: " << db["taxedby"] << "</td></tr>" << endl;
+
+  cout << "<tr><td>Ratios</td><td>Buy: " << db["profit_buy"];
+  cout << "</td><td>Sell: " << db["profit_sell"] << "</td></tr>" << endl;
+
+  cout << "<tr><td>Reserve</td><td>Min: " << db["reserve_min"] << "</td>";
+  cout << "<td>Max: " << db["reserve_max"] << "</td></tr>" << endl;
+
+  cout << "<tr><td>Dividend</td><td>" << db["dividend"] << "</td></tr>" <<endl;
+
+  cout << "<tr><td>Max Inv.</td><td>" << db["max_num"] << "</td></tr>" << endl;
+
+  cout << "<tr><td colspan=3 align=center>Custom Strings</td></tr>" << endl;
+
+  if(!db["no_such_item1"].empty()){
+    cout << "<tr><td>no_such_item1</td><td colspan=2>" << db["no_such_item1"];
+    cout << "</td></tr>" << endl;
+  }
+  if(!db["no_such_item2"].empty()){
+    cout << "<tr><td>no_such_item2</td><td colspan=2>" << db["no_such_item2"];
+    cout << "</td></tr>" << endl;
+  }
+  if(!db["do_not_buy"].empty()){
+    cout << "<tr><td>do_not_buy</td><td colspan=2>" << db["do_not_buy"];
+    cout << "</td></tr>" << endl;
+  }
+  if(!db["missing_cash1"].empty()){
+    cout << "<tr><td>missing_cash1</td><td colspan=2>" << db["missing_cash1"];
+    cout << "</td></tr>" << endl;
+  }
+  if(!db["missing_cash2"].empty()){
+    cout << "<tr><td>missing_cash2</td><td colspan=2>" << db["missing_cash2"];
+    cout << "</td></tr>" << endl;
+  }
+  if(!db["message_buy"].empty()){
+    cout << "<tr><td>message_buy</td><td colspan=2>" << db["message_buy"];
+    cout << "</td></tr>" << endl;
+  }
+  if(!db["message_sell"].empty()){
+    cout << "<tr><td>message_sell</td><td colspan=2>" << db["message_sell"];
+    cout << "</td></tr>" << endl;
+  }
+
+  
+  cout << "</table>" << endl;
+  
+
+  cout << body() << endl;
+  cout << html() << endl; 
+}
+
 void sendShoplist(int account_id){
   TDatabase db(DB_SNEEZY);
 
@@ -85,6 +183,7 @@ void sendShoplist(int account_id){
 
   cout << HTTPHTMLHeader() << endl;
   cout << html() << head() << title("Shopinfo") << endl;
+  sendJavaScript();
   cout << head() << body() << endl;
 
   cout << "<form method=post action=shopinfo.cgi>" << endl;
