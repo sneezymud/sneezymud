1e6dc3b379c96169e7862e9789122e0d56bc34a4
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2006-04-11 08:09:37 +0000

    added rebooter scripts to subversion under code directory probably a better place for this, but we need to get them under source control also added "rebooter" target to makefile, which just copies it to /mud/prod

---
+++ code/rebooter-watchdog.sh
@@ -0,0 +1,48 @@
+#!/usr/local/bin/bash
+#
+# SneezyMUD rebooter watchdog script, run via cron
+#
+# January, 2005
+
+cd /mud/prod
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
+export PATH
+
+if [ -r rebooter.cfg ]
+then
+  source ./rebooter.cfg
+else
+  echo "Error: $(basename $0) can't read in rebooter.cfg"
+  exit 1
+fi
+
+REBOOTER_WATCHDOG_LOG=${PROD_DIR}/rebooter-watchdog.log
+#running_pid=$(pgrep -U sneezy -f "/usr/local/bin/bash .*${REBOOTER}")
+
+if [ -e "${PROD_LOCK}" ]
+then
+  DATE_STAMP=$(date +%Y%m%d.%H%M)
+  echo "${DATE_STAMP}: Lock file ${PROD_LOCK} found, not starting ${REBOOTER}" >> ${REBOOTER_WATCHDOG_LOG}
+  exit 0
+fi
+
+if [ -s "${REBOOTER_PID}" ]
+then
+  stored_pid=$(cat "${REBOOTER_PID}")
+  verify_pid=$(ps ax | awk '{print $1}' | egrep "^${stored_pid}$")
+  if [ "$stored_pid" != "$verify_pid" ]
+  then
+    # we have a problem, REBOOTER_PID isn't running anymore
+    DATE_STAMP=$(date +%Y%m%d.%H%M)
+    echo "${DATE_STAMP}: ${REBOOTER}, pid=$stored_pid was dead!  Spawning new rebooter." >> ${REBOOTER_WATCHDOG_LOG}
+    exec ${PROD_DIR}/${REBOOTER} &
+  fi
+else
+  # $REBOOTER_PID is not here or empty at this point, we have a problem
+  DATE_STAMP=$(date +%Y%m%d.%H%M)
+  echo "${DATE_STAMP}: ${REBOOTER} wasn't running!  Spawning new rebooter." >> ${REBOOTER_WATCHDOG_LOG}
+  exec "${PROD_DIR}/${REBOOTER}" &
+fi
+
+exit 0
