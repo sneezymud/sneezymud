228748988db4ddb7d615f4c0da47fbdf82543925
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2006-05-08 16:47:50 +0000

    fixed query that was incompatible with mysql

--- code/spec_mobs_property_clerk.cc
+++ code/spec_mobs_property_clerk.cc
@@ -13,7 +13,7 @@ int propertyClerk(TBeing *ch, cmdTypeT cmd, const char *argument, TMonster *me, 
     if(arg.empty()){
       me->doTell(ch->getName(), "Which property do you want to buy a replacement key for?");
     } else {
-      db.query("select p.owner as owner_id, p.key_vnum as key from property p, obj o where p.key_vnum=o.vnum and p.id=%s", arg.word(0).c_str());
+      db.query("select p.owner as owner_id, p.key_vnum as key_vnum from property p, obj o where p.key_vnum=o.vnum and p.id=%s", arg.word(0).c_str());
 
       db.fetchRow();
 
@@ -29,7 +29,7 @@ int propertyClerk(TBeing *ch, cmdTypeT cmd, const char *argument, TMonster *me, 
 	  ch->addToMoney(-25000, GOLD_SHOP);
 	}
 
-	TObj *key=read_object(convertTo<int>(db["key"]), VIRTUAL);
+	TObj *key=read_object(convertTo<int>(db["key_vnum"]), VIRTUAL);
 	*ch += *key;
 	me->doTell(ch->getName(), "Here you go!");
 	act("$n gives you $p.", 0, me, key, ch, TO_VICT);
@@ -47,7 +47,7 @@ int propertyClerk(TBeing *ch, cmdTypeT cmd, const char *argument, TMonster *me, 
 		   convertTo<int>(db["id"]) % db["name"]);
       }
     } else {
-      db.query("select p.id as id, p.name as name, p.owner as owner_id, pl.name as owner, p.key_vnum as key, o.short_desc as keyname, p.entrance as entrance_id, r.name as entrance from property p, player pl, obj o, room r where p.owner=pl.id and p.key_vnum=o.vnum and r.vnum=p.entrance and p.id=%s", arg.word(0).c_str());
+      db.query("select p.id as id, p.name as name, p.owner as owner_id, pl.name as owner, p.key_vnum as key_vnum, o.short_desc as keyname, p.entrance as entrance_id, r.name as entrance from property p, player pl, obj o, room r where p.owner=pl.id and p.key_vnum=o.vnum and r.vnum=p.entrance and p.id=%s", arg.word(0).c_str());
       
       while(db.fetchRow()){
 	if(ch->isImmortal()){
@@ -56,7 +56,7 @@ int propertyClerk(TBeing *ch, cmdTypeT cmd, const char *argument, TMonster *me, 
 	  me->doTell(ch->getName(), fmt("Owned by: %s (%i)") % 
 		     db["owner"] % convertTo<int>(db["owner_id"]));
 	  me->doTell(ch->getName(), fmt("Key: %s (%i)") % 
-		     db["keyname"] % convertTo<int>(db["key"]));
+		     db["keyname"] % convertTo<int>(db["key_vnum"]));
 	  me->doTell(ch->getName(), fmt("Entrance: %s (%i)") %
 		     db["entrance"] % convertTo<int>(db["entrance_id"]));
 	} else {
