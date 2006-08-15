67f51d04b63a8e962badaaf68f219d4a486198a7
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2006-08-15 11:34:08 +0000

    this is a mirror of shoplog, for archival purposes

---
+++ code/sql/sneezy/shoplogarchive.sql
@@ -0,0 +1,11 @@
+CREATE TABLE shoplogarchive (
+    shop_nr integer,
+    name varchar(80),
+    action varchar(32),
+    item varchar(80),
+    talens integer,
+    shoptalens integer,
+    shopvalue integer,
+    logtime timestamp,
+    itemcount integer
+);
