e03d1b6a33a6857e2bf9f23e66d2100012dbdf27
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2006-09-28 16:35:09 +0000

    initial checkin

---
+++ code/sql/sneezy/shoplog_retained_earnings.sql
@@ -0,0 +1,4 @@
+create table shoplog_retained_earnings (
+  shop_nr int,
+  retained_earnings int
+);
---
+++ code/sql/sneezy/shoplogjournalarchive.sql
@@ -0,0 +1,13 @@
+myslcreate table shoplogjournal (
+  shop_nr int,
+  journal_id int auto_increment,
+  customer_name text,
+  obj_name text,
+  sneezy_year int,
+  logtime timestamp,
+  post_ref int,
+  debit int,
+  credit int,
+  key (journal_id)
+);
+
