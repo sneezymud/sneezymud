f751ce79e9803f92c5d1918a8e4bc6ecac9eabd1
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2007-10-07 08:43:48 +0000

    non-humanoids no longer suffer foot damage

--- code/equip.cc
+++ code/equip.cc
@@ -1888,6 +1888,9 @@ void TBeing::wearNTear(void)
 	    i == WEAR_EX_FOOT_R || i == WEAR_EX_FOOT_L) {
           if (isLevitating())
             return; // If they are levitating, they will not hurt their feet.
+	  // animals and such don't have this problem
+	  if(!isHumanoid())
+	    return;
 
           act("<r>Your feet are getting sore from walking around barefoot.<1>",
 	      TRUE,this,NULL,0,TO_CHAR);
