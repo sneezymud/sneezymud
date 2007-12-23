43476ca82a62123b5bb8088eee972e8db4aaa7ba
pappy <pappy@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2007-12-23 06:21:49 +0000

    -Fixed a bad null check aded when I added extra logging for item scrapping

--- code/combat.cc
+++ code/combat.cc
@@ -1246,7 +1246,7 @@ void TObj::makeScraps()
     act(buf, TRUE, ch, this, NULL, TO_ROOM);
     act(buf2, FALSE, ch, this, NULL, TO_CHAR, ANSI_RED);
 
-    vlogf(LOG_COMBAT, fmt("%s's %s just scrapped.") %  ch->getName() % getName());
+    vlogf(LOG_COMBAT, fmt("%s's %s just scrapped.") %  (ch ? ch->getName() : "Nobodys") % getName());
   } else {
     if ((parent && (tmp = parent->equippedBy)) || (tmp = parent))  {
       if (isMineral()) {
@@ -1256,7 +1256,7 @@ void TObj::makeScraps()
         sprintf(buf, "$p is destroyed.");
         sprintf(buf2, "Your $o is destroyed.");
       }
-      vlogf(LOG_COMBAT, fmt("%s's %s just scrapped.") %  ch->getName() % getName());
+      vlogf(LOG_COMBAT, fmt("%s's %s just scrapped.") %  (ch ? ch->getName() : "Nobodys") % getName());
       while (tmp) {
         if (tmp->roomp) {
           act(buf2, TRUE, tmp, this, NULL, TO_CHAR, ANSI_RED);
@@ -1271,7 +1271,7 @@ void TObj::makeScraps()
       else
         sprintf(buf, "$n is destroyed.");
       act(buf, TRUE, this, NULL, NULL, TO_ROOM);
-      vlogf(LOG_COMBAT, fmt("%s's %s just scrapped.") %  ch->getName() % getName());
+      vlogf(LOG_COMBAT, fmt("%s's %s just scrapped.") %  (ch ? ch->getName() : "Nobodys") % getName());
     } else 
       vlogf(LOG_COMBAT, fmt("Something in make scraps isnt in a room %s.") %  getName());
   }
