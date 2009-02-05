809da4786bb252754c929b38a92eba34712e7fed
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2009-02-05 08:49:57 +0000

    fixed a typo in log formatting

--- code/utility.cc
+++ code/utility.cc
@@ -811,7 +811,10 @@ void vlogf(logTypeT tError, const sstring &errorMsg)
   buf=getLogType(tError);
   name=getLogPlayer(tError);
 
-  buf = fmt("%s: %s") % buf % errorMsg;
+  if(buf=="")
+    buf=errorMsg;
+  else
+    buf = fmt("%s: %s") % buf % errorMsg;
 
   fprintf(stderr, "%4.4d|%2.2d%2.2d|%2.2d:%2.2d:%2.2d :: %s\n",
           this_time->tm_year + 1900, this_time->tm_mon + 1, this_time->tm_mday,
