3983bec919ab24d61fed866b0d008f0f1f8b426f
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2009-03-16 11:01:18 +0000

    simple test for act()

---
+++ code/tests/Act.h
@@ -0,0 +1,75 @@
+#include <cxxtest/TestSuite.h>
+
+#include "stdsneezy.h"
+#include "tests/ValueTraits.h"
+#include "connect.h"
+#include "socket.h"
+
+class Act : public CxxTest::TestSuite
+{
+ public:
+  TSocket *testSocket;
+  Descriptor *testDesc;
+  Descriptor *testDesc2;
+  Descriptor *testHorseDesc;
+  TPerson *testPerson;
+  TPerson *testPerson2;
+  TPerson *testHorse;
+  TRoom *testRoom;
+  charFile st;
+
+  void setUp(){
+    buildSpellArray();
+    chdir("lib");
+    Races[RACE_HUMAN] = new Race(RACE_HUMAN);    
+
+    testSocket=new TSocket();
+    testRoom=new TRoom(100);
+    testRoom->setRoomFlagBit(ROOM_ALWAYS_LIT);
+
+    testDesc=new Descriptor(testSocket);
+    testPerson=new TPerson(testDesc);
+    load_char("dante", &st);
+    testPerson->loadFromSt(&st);
+    testPerson->in_room=0;
+    *testRoom += *testPerson;
+
+    testDesc2=new Descriptor(testSocket);
+    testPerson2=new TPerson(testDesc2);
+    load_char("milton", &st);
+    testPerson2->loadFromSt(&st);
+    testPerson2->in_room=0;
+    *testRoom += *testPerson2;
+
+    testHorseDesc=new Descriptor(testSocket);
+    testHorse=new TPerson(testHorseDesc);
+    load_char("chaucer", &st);
+    testHorse->loadFromSt(&st);
+    testHorse->in_room=0;
+    *testRoom += *testHorse;
+  }
+
+  void testSimple(){
+    Comm *c;
+
+    act("You start riding $N.", FALSE, testPerson, 0, testHorse, TO_CHAR, NULL, -1);
+    act("$n starts riding $N.", FALSE, testPerson, 0, testHorse, TO_NOTVICT, NULL, -1);
+    act("$n hops on your back!", FALSE, testPerson, 0, testHorse, TO_VICT, NULL, -1);
+
+    if(!(c=testPerson->desc->output.takeFromQ()))
+      TS_FAIL("received NULL from output queue");
+    else
+      TS_ASSERT_EQUALS(c->getComm(COMM_TEXT), "You start riding Chaucer.\n\r");
+
+    if(!(c=testPerson2->desc->output.takeFromQ()))
+      TS_FAIL("received NULL from output queue");
+    else
+      TS_ASSERT_EQUALS(c->getComm(COMM_TEXT), "Dante starts riding Chaucer.\n\r");
+
+    if(!(c=testHorse->desc->output.takeFromQ()))
+      TS_FAIL("received NULL from output queue");
+    else
+      TS_ASSERT_EQUALS(c->getComm(COMM_TEXT), "Dante hops on your back!\n\r");
+
+  }
+};
--- code/tests/testlist.mk
+++ code/tests/testlist.mk
@@ -1,5 +1,5 @@
 TESTS=	DBTest.h CommTest.h Format.h SString.h Exceptions.h ConvertTo.h \
-	Garble.h
+	Garble.h Act.h
 
 TESTS := $(sort $(addprefix tests/, $(TESTS)))
 
