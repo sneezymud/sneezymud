4beb34cefef531d8383d288d3ae7a5dd6dea5944
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2010-02-05 17:39:52 +0000

    updated for use with new configuration file setup

--- code/code/tests/template.h
+++ code/code/tests/template.h
@@ -1,11 +1,21 @@
 #include <cxxtest/TestSuite.h>
 
 #include "code/tests/ValueTraits.h"
+#include "configuration.h"
+//#include "extern.h"
+//#include "toggle.h"
 
 class template : public CxxTest::TestSuite
 {
 
  public:
+  void setUp(){
+    Config::doConfiguration();
+    freopen("code/tests/output/template.out", "w", stderr);
+    //    generate_obj_index();
+    //    toggleInfo.loadToggles();
+  }
+
   void testAddition(){
     TS_ASSERT_EQUALS(1+1, 2);
   }
