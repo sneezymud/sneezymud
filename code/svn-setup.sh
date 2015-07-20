b1d22dff2b8ad73138eeb6b033e0d0ecaadf4a0a
Ryan Smith-Roberts <rmsr@lab.net>
2015-07-20 12:52:34 -0700

    Remove some stale shellscripts etc from /code

--- code/svn-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-SVNROOT="http://svn.sneezymud.com/repos/source/trunk/code"
-cd $HOME
-svn checkout $SVNROOT sneezy
-ln -s /mud/build/lib sneezy/lib
-cp sneezy/sneezy_beta.cfg sneezy/sneezy.cfg
-echo "Your dev environment is now in $HOME/sneezy."
-echo "An SVN tutorial is available: $HOME/sneezy/svntutorial.txt"
