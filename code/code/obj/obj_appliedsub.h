08964492f6663b4f833d6987dbb7825baf9dcf0c
Ryan Smith-Roberts <rmsr@lab.net>
2015-07-22 19:03:02 -0700

    Strip TASubstance down to a stub
    I'd remove it but there's still a user in the database, and we don't have db
    migration yet *coughs*

--- code/code/obj/obj_appliedsub.h
+++ code/code/obj/obj_appliedsub.h
@@ -1,45 +1,26 @@
-//////////////////////////////////////////////////////////////////////////
-//
-// SneezyMUD - All rights reserved, SneezyMUD Coding Team
-//
-//
-//////////////////////////////////////////////////////////////////////////
-
 #ifndef __OBJ_APPLIED_SUB_H
 #define __OBJ_APPLIED_SUB_H
 
 #include "obj.h"
 
+// stub object relating to ranger herbalism
 
 class TASubstance : public TObj {
-  private:
-    int AFlags,
-        AMethod,
-        ILevel,
-        ISpell;
   public:
-    virtual void assignFourValues(int, int, int, int);
-    virtual void getFourValues(int *, int *, int *, int *) const;
-    virtual sstring statObjInfo() const;
+    virtual void assignFourValues(int, int, int, int) { }
+    virtual void getFourValues(int *a, int *b, int *c, int *d) const { *a = *b = *c = *d = 0; }
+    virtual sstring statObjInfo() const { return sstring(""); }
+
     virtual itemTypeT itemType() const {return ITEM_APPLIED_SUB; }
 
-    virtual void describeObjectSpecifics(const TBeing *) const;
-    virtual int  applyMe(TBeing *, TThing *);
-    void setAFlags(int);
-    int getAFlags() const;
-    void setAMethod(int);
-    int getAMethod() const;
-    void setILevel(int);
-    int getILevel() const;
-    void setISpell(int);
-    int getISpell() const;
-
-    TASubstance();
-    TASubstance(const TASubstance &a);
-    TASubstance & operator=(const TASubstance &a);
-    virtual ~TASubstance();
+    TASubstance() { }
+    TASubstance(const TASubstance &a) { }
+    TASubstance & operator=(const TASubstance &a) {
+      if (this == &a) return *this;
+      TObj::operator=(a);
+      return *this;
+    }
+    virtual ~TASubstance() { }
 };
-
-
 
 #endif
