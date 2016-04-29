052b47bbd72e898ce02d89afd4963c88c0211709
GrimhavenMUD <support@grimhavenmud.com>
2016-04-29 06:27:38 -0500

    Switched MUD name back to SneezyMUD

--- code/code/sys/db.h
+++ code/code/sys/db.h
@@ -67,8 +67,8 @@ class Path {
   static const char * const SPELL_HELP;
 };  
 
-const char * const MUDADMIN_EMAIL   ="support@grimhavenmud.com";
-const char * const CODERS_EMAIL     ="support@grimhavenmud.com";
+const char * const MUDADMIN_EMAIL   ="support@sneezymud.com";
+const char * const CODERS_EMAIL     ="support@sneezymud.com";
 
 const int WORLD_SIZE = 50000;
 const int ZONE_ROOM_RANDOM = -99;
