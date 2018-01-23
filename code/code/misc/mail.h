f41787d9bfa9ce64c72d682d17dda52df5bf8d6c
Ryan Smith-Roberts <rmsr@lab.net>
2018-01-23 02:45:02 -0800

    Various mudmail cleanups
    * can't mudmail to nonexistent users any more (only 15 years to fix!)
    * can't mudmail zero talens
    * postmaster tell & flavor improvements
    * sql query reduction
    * improved bug logging
    * sstringification
    * formatting cleanups

--- code/code/misc/mail.h
+++ code/code/misc/mail.h
@@ -8,15 +8,9 @@
 #ifndef __MAIL_H
 #define __MAIL_H
 
-const int MIN_MAIL_LEVEL=2;
-const int STAMP_PRICE=50;
-const int FACTION_STAMP_PRICE=5000;
-const int MAX_MAIL_SIZE=4000;
-const int NAME_SIZE =15;
-
+const int MAX_MAIL_SIZE = 4000;
 
 extern bool has_mail(const sstring);
-
 extern void autoMail(TBeing *, const char *, const char *, int m = 0, int r = 0);
 
 #endif
