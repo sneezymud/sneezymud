c26614a71985eb13e15318d7a7b663c60a0db3ca
Elmo Todurov <elmo.todurov@eesti.ee>
2018-07-11 06:25:33 +0000

    Enabled promoting level50 accounts to double/triples

--- code/code/misc/limits.cc
+++ code/code/misc/limits.cc
@@ -647,7 +647,6 @@ void TPerson::advanceLevel(classIndT Class)
   if (desc && GetMaxLevel() >= 50) {
     if (desc->career.hit_level50 == 0)
       desc->career.hit_level50 = time(0);
-#if 0
     if (isSingleClass()) {
       SET_BIT(desc->account->flags, ACCOUNT_ALLOW_DOUBLECLASS);
       sendTo(COLOR_BASIC, "<r>Congratulations on obtaining L50!<z>\n\rYou may now create <y>double-class characters<z>!\n\r");
@@ -658,7 +657,6 @@ void TPerson::advanceLevel(classIndT Class)
       sendTo(COLOR_BASIC, "<r>Congratulations on obtaining L50!<z>\n\rYou may now create <y>triple-class characters<z>!\n\r");
       desc->saveAccount();
     }
-#endif
   }
 
   doHPGainForLev(Class);
