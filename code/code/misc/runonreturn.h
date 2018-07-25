7bf36036e6cc512bfccd8e430c7dac706df82394
Elmo Todurov <elmo.todurov@eesti.ee>
2018-07-25 20:38:13 +0000

    Speeded up restocking shops by wrapping it into a transaction

---
+++ code/code/misc/runonreturn.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class RunOnReturn
+{
+  private:
+    std::function<void(void)>&& fn;
+  public:
+    RunOnReturn(std::function<void(void)>&& fn)
+      : fn(std::move(fn))
+    { }
+    ~RunOnReturn()
+    {
+      fn();
+    }
+};
