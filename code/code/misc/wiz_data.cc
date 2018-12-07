9d160de2d8932a553c3b0b7381edeaaa07d90fdc
Elmo Todurov <elmo.todurov@eesti.ee>
2018-12-07 12:10:07 +0200

    Revert "migrate wiz data files to db"
    This reverts commit f4305c176c2b96af161367ced1952c705d07b2ea.

--- code/code/misc/wiz_data.cc
+++ code/code/misc/wiz_data.cc
@@ -16,29 +16,68 @@ extern "C" {
 #include "extern.h"
 #include "monster.h"
 #include "person.h"
-#include "database.h"
+
+class wizSaveData {
+  public:
+    int setsev,
+        office,
+        blockastart,
+        blockaend,
+        blockbstart,
+        blockbend;
+
+    wizSaveData();
+};
+
+wizSaveData::wizSaveData() :
+  setsev(0),
+  office(0),
+  blockastart(0),
+  blockaend(0),
+  blockbstart(0),
+  blockbend(0)
+{}
 
 void TBeing::wizFileRead()
 {
+  FILE *fp;
+  sstring buf, buf2;
   Descriptor *d = NULL;
+  wizSaveData saveData;
 
   // don't use isImmortal, save always
   if (!(GetMaxLevel() > MAX_MORT) || !(d = desc))
     return;
 
-  TDatabase db(DB_SNEEZY);
+  buf = format("immortals/%s/wizdata") % getName();
+  fp = fopen(buf.c_str(), "r");
+  if (!fp) {
+    buf2 = format("immortals/%s") % getName();
+    fp = fopen(buf2.c_str(), "r");
+    if (!fp) {	// no immort directory 
+      if (mkdir(buf2.c_str(), 0770)) {
+	sendTo("Unable to create a wizard directory for you.\n\r");
+	vlogf(LOG_FILE, format("Unable to create a wizard directory for %s.") %  getName());
+      } else
+	sendTo("Wizard directory created...\n\r");
+    } else
+      fclose(fp);	// player has no wizard data, but has immort directory 
 
-  db.query("select setsev, office, blockastart, blockaend, blockbstart, blockbend, player_id from wizdata "
-           "where player_id = %i", getPlayerID());
+    return;
+  }
+  if (fread(&saveData, sizeof(saveData), 1, fp) != 1) {
+    vlogf(LOG_BUG, format("Corrupt wiz save file for %s") %  getName());
+    fclose(fp);
+    return;
+  } 
+  fclose(fp);
 
-  db.fetchRow();
-
-  d->severity     = (int) convertTo<int>(db["setsev"]);
-  d->office       = (int) convertTo<int>(db["office"]);
-  d->blockastart  = (int) convertTo<int>(db["blockastart"]);
-  d->blockaend    = (int) convertTo<int>(db["blockaend"]);
-  d->blockbstart  = (int) convertTo<int>(db["blockbstart"]);
-  d->blockbend    = (int) convertTo<int>(db["blockbend"]);
+  d->severity    = saveData.setsev;
+  d->office      = saveData.office;
+  d->blockastart = saveData.blockastart;
+  d->blockaend   = saveData.blockaend;
+  d->blockbstart = saveData.blockbstart;
+  d->blockbend   = saveData.blockbend;
 
   if (should_be_logged(this))
     vlogf(LOG_IIO, format("Loaded %s's wizard file.") %  getName());
@@ -64,27 +103,52 @@ void TMonster::wizFileSave()
 
 void TPerson::wizFileSave()
 {
+  FILE *fp;
+  sstring buf;
   Descriptor *d = NULL;
+  wizSaveData saveData;
 
   if (!(d = desc))
     return;
   if (d->connected != CON_PLYNG)  // semi arbitrary, but here for sanity
     return;
 
-  TDatabase db(DB_SNEEZY);
+  buf = format("immortals/%s/wizdata") % getName();
+  unlink(buf.c_str());
+  if (!(fp = fopen(buf.c_str(), "wa+"))) {
+    buf = format("immortals/%s") % getName();
+    if (!(fp = fopen(buf.c_str(), "r"))) {	// no immort directory 
+      if (mkdir(buf.c_str(), 0770))
+	sendTo("Unable to create a wizard directory for you.  Tell Brutius or Batopr.\n\r");
+      else
+	sendTo("Wizard directory created... type 'save' again.\n\r");
+    } else
+      fclose(fp);	// player has no wizard data, but has immort directory 
 
-  db.query("replace into wizdata (setsev, office, blockastart, blockaend, blockbstart, blockbend, player_id) "
-            "values (%i, %i, %i,%i, %i, %i, %i)", d->severity, d->office, d->blockastart, 
-            d->blockaend, d->blockbstart, d->blockbend, getPlayerID());
+    return;
+  }
 
+  saveData.setsev      = d->severity;
+  saveData.office      = d->office;
+  saveData.blockastart = d->blockastart;
+  saveData.blockaend   = d->blockaend;
+  saveData.blockbstart = d->blockbstart;
+  saveData.blockbend   = d->blockbend;
+
+  fwrite(&saveData, sizeof(wizSaveData), 1, fp);   
+  fclose(fp);
 }
 
 
 void TBeing::doOffice(sstring arg)
 {
-  sstring buf;
+  FILE *fp;
+  sstring buf, buf2, buf3;
+  Descriptor *d = NULL;
+  wizSaveData saveData;
+
   // don't use isImmortal, save always
-  if (!(GetMaxLevel() > MAX_MORT) || !hasWizPower(POWER_GOTO))
+  if (!(GetMaxLevel() > MAX_MORT) || !(d = desc) || !hasWizPower(POWER_GOTO))
     return;
 
   if (arg == "") {
@@ -92,11 +156,19 @@ void TBeing::doOffice(sstring arg)
     return;
   }
 
-  TDatabase db(DB_SNEEZY);
-
-  db.query("select office from wizdata where player_id = (select id from player where name = '%s')", arg.c_str());
-  db.fetchRow();
-  buf = format("The office of %s is %d.\n\r") % arg % (int)convertTo<int>(db["office"]);
-  sendTo(buf);
+  buf = format("immortals/%s/wizdata") % arg;
+  fp = fopen(buf.c_str(), "r");
+  if (!fp) {
+	  sendTo(format("Unable to open file for %s (case sensitive!).\n\r") % arg);
+    return;
+  }
+  if (fread(&saveData, sizeof(saveData), 1, fp) != 1) {
+    vlogf(LOG_BUG, format("Corrupt wiz save file for %s") % arg);
+    fclose(fp);
+    return;
+  } 
+  fclose(fp);
+  buf3 = format("The office of %s is %d.\n\r") % arg % saveData.office;
+  sendTo(buf3);
 }
 
