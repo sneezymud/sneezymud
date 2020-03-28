d6a8597aa341f2ff4a4f90925c750403d1960313
Jeff Wolak <jeffrey.wolak@gmail.com>
2020-03-27 23:15:00 -0700

    some multiclass fixes

--- code/code/misc/other.cc
+++ code/code/misc/other.cc
@@ -569,6 +569,7 @@ void TBeing::doSplit(const char *argument, bool tell)
 void TBeing::doReport(const char *argument)
 {
   char info[256];
+  char inter[256];
   char target[80];
   TThing *t;
   int found = 0;
@@ -586,22 +587,22 @@ void TBeing::doReport(const char *argument)
     return;
   }
   strncpy(target, argument, sizeof(target));
-  if (hasClass(CLASS_CLERIC) || hasClass(CLASS_DEIKHAN))
-    sprintf(info, "$n reports '%s%.1f%% H, %.2f%% P. I am %s%s'",
-           red(), getPercHit(), getPiety(),
-           DescMoves((((double) getMove()) / ((double) moveLimit()))),
-           norm());
-  else if (hasClass(CLASS_SHAMAN))
-    sprintf(info, "$n reports '%s%.1f%% H, %-4d LF. I am %s%s'",
-           red(), getPercHit(), getLifeforce(),
-           DescMoves((((double) getMove()) / ((double) moveLimit()))),
-           norm());
-  else
-    sprintf(info, "$n reports '%s%.1f%% H, %.1f%% M. I am %s%s'", 
-           red(), getPercHit(), getPercMana(), 
-           DescMoves((((double) getMove()) / ((double) moveLimit()))), 
-           norm());
 
+  sprintf(info, "%.1f%% H, ", getPercHit());
+  if (hasClass(CLASS_CLERIC) || hasClass(CLASS_DEIKHAN)) {
+    sprintf(inter, "%.2f%% P, ", getPiety());
+    strcat(info, inter);
+  }
+  if (hasClass(CLASS_SHAMAN)) {
+    sprintf(inter, "%-4d LF, ", getLifeforce());
+    strcat(info, inter);
+  }
+  if (hasClass(CLASS_MAGE) || hasClass(CLASS_MONK) || hasQuestBit(TOG_PSIONICIST)) {
+    sprintf(inter, "%.1f%% M, ", getPercMana());
+    strcat(info, inter);
+  }
+  sprintf(inter, "I am %s", DescMoves((((double) getMove()) / ((double) moveLimit()))));
+  strcat(info, inter);
 
   for(StuffIter it=roomp->stuff.begin();it!=roomp->stuff.end();){
     t=*(it++);
@@ -622,26 +623,13 @@ void TBeing::doReport(const char *argument)
       return;
     }
     sendTo(COLOR_MOBS, format("You report your status to %s.\n\r") % targ->getName());
-    if (hasClass(CLASS_CLERIC) || hasClass(CLASS_DEIKHAN))
-      sprintf(info, "<G>$n directly reports to you  '%s%.1f%% H, %.2f%% P. I am %s%s'<1>",
-           red(), getPercHit(), getPiety(),
-           DescMoves((((double) getMove()) / ((double) moveLimit()))),
-           norm());
-    else if (hasClass(CLASS_SHAMAN))
-      sprintf(info, "<G>$n directly reports to you  '%s%.1f%% H, %-4d LF. I am %s%s'<1>",
-           red(), getPercHit(), getLifeforce(),
-           DescMoves((((double) getMove()) / ((double) moveLimit()))),
-           norm());
-    else
-      sprintf(info, "<G>$n directly reports to you '%s%.1f%% H, %.1f%% M. I am %s%s'<1>",
-           red(), getPercHit(), getPercMana(),
-           DescMoves((((double) getMove()) / ((double) moveLimit()))),
-           norm());
 
-    colorAct(COLOR_COMM, info, FALSE, this, 0, targ, TO_VICT);
+    sprintf(inter, "<G>$n directly reports to you '%s%s%s'<1>", red(), info, norm());
+    colorAct(COLOR_COMM, inter, FALSE, this, 0, targ, TO_VICT);
     disturbMeditation(targ);
   } else {
-    act(info, FALSE, this, 0, 0, TO_ROOM);
+    sprintf(inter, "$n reports '%s%s%s'", red(), info, norm());
+    act(inter, FALSE, this, 0, 0, TO_ROOM);
     sendTo("You report your status to the room.\n\r");
   }
 }
@@ -1531,6 +1519,7 @@ void TPerson::doFeedback(const sstring &type, int clientCmd, const sstring &arg)
   }
 }
 
+// TODO: this needs fixing for multiclass
 void TBeing::doGroup(const char *argument, bool silent)
 {
   char namebuf[256];
