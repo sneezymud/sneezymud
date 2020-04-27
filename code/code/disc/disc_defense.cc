d3f04b7330a2a98b307c768602f4c15b7d8bec48
Jeff Wolak <jeffrey.wolak@gmail.com>
2020-04-27 07:42:21 -0700

    start to add checks for focused avoidance

--- code/code/disc/disc_defense.cc
+++ code/code/disc/disc_defense.cc
@@ -6,6 +6,8 @@
 
 
 #include "disc_defense.h"
+#include "being.h"
+
 
 CDDefense::CDDefense() :
   CDiscipline(),
@@ -32,4 +34,30 @@ CDDefense & CDDefense::operator=(const CDDefense &a)
 
 CDDefense::~CDDefense()
 {
+}
+
+// The larger perc is passed in the harder it is to avoid
+bool TBeing::canFocusedAvoidance(int perc) {
+
+  if (!doesKnowSkill(SKILL_FOCUSED_AVOIDANCE))
+    return FALSE;
+
+  if (!awake() || getPosition() < POSITION_CRAWLING)
+    return FALSE;
+
+  int skill = getSkillValue(SKILL_FOCUSED_AVOIDANCE);
+
+  // Agi seems to be the defense stat so this makes sense
+  skill *= getAgiMod();
+
+  if (eitherLegHurt())
+    skill = (int) (skill * 0.75);
+
+  skill -= perc;
+
+  // bSuccess is modified by focus
+  if (!bSuccess(skill, SKILL_FOCUSED_AVOIDANCE))
+    return FALSE;
+
+  return TRUE;
 }
