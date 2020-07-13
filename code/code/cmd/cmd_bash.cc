fffa4e18d0a6e3b2daac533c54f3d55e820567bb
Jeff Wolak <jeffrey.wolak@gmail.com>
2020-07-13 01:02:34 -0700

    remove comment

--- code/code/cmd/cmd_bash.cc
+++ code/code/cmd/cmd_bash.cc
@@ -323,8 +323,6 @@ int TBeing::bashSuccess(TBeing *victim, spellNumT skill)
   // the effect here should be strictly to prevent skill-use
   // it should NOT cause loss of attacks, or do damage
   float wt = combatRound(discArray[SKILL_BASH]->lag);
-  // adjust based on bash difficulty
-  // wt = (wt * 100.0 / getSkillDiffModifier(SKILL_BASH));
 
   // Take a few moves from basher
   // since we cost some moves to perform, allow an extra 1/2 round of lag
