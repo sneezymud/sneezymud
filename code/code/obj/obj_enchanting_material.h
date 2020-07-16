1c62713d8098748814be0ea1ec65a1705c796146
Aion <33821745+Aion2501@users.noreply.github.com>
2020-07-16 13:18:47 -0700

    Creating obj_enchanting_material.h
    Based on treasure.h file.

---
+++ code/code/obj/obj_enchanting_material.h
@@ -0,0 +1,37 @@
+//////////////////////////////////////////////////////////////////////////
+//
+// SneezyMUD - All rights reserved, SneezyMUD Coding Team
+//
+//
+//////////////////////////////////////////////////////////////////////////
+
+// based on treasure.h
+
+#ifndef __OBJ_ENCHANTING_MATERIAL_H
+#define __OBJ_ENCHANTING_MATERIAL_H
+
+#include "obj.h"
+
+class TTreasure : public TObj {
+  private:
+    int serial_number;
+  public:
+    virtual void assignFourValues(int, int, int, int);
+    virtual void getFourValues(int *, int *, int *, int *) const;
+    virtual sstring statObjInfo() const;
+    virtual itemTypeT itemType() const { return ITEM_ENCHANTING_MATERIAL; }
+
+    virtual int scavengeMe(TBeing *, TObj **);
+
+    void setSerialNumber(int s){ serial_number = s; }
+    int getSerialNumber() const { return serial_number; }
+    virtual void lowCheck();
+    
+    TTreasure();
+    TTreasure(const TTreasure &a);
+    TTreasure & operator=(const TTreasure &a);
+    virtual ~TTreasure();
+};
+
+
+#endif
