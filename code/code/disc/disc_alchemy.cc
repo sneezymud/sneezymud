3be98bd101c516826878a90354f9f7414a2fb097
Jeff Wolak <jeffrey.wolak@gmail.com>
2021-03-15 23:37:26 -0700

    add gate

--- code/code/disc/disc_alchemy.cc
+++ code/code/disc/disc_alchemy.cc
@@ -23,6 +23,7 @@
 #include "obj_opal.h"
 #include "obj_component.h"
 #include "obj_magic_item.h"
+#include "obj_portal.h"
 #include "obj_scroll.h"
 #include "person.h"
 #include "pathfinder.h"
@@ -2208,28 +2209,109 @@ int castGalvanize(TBeing *caster, TObj *obj)
   return FALSE;
 }
 
-int ethrealGate(TBeing *caster, TObj *obj, int, short bKnown)
+struct gateRoomT {
+      int roomnum;
+      const char *name;
+};
+
+gateRoomT gateRooms[] =
 {
-  return SPELL_FALSE;
+  {15346, "grimhaven"},
+  {11000, "cloud"},
+  {5700, "auria"},
+  {6953, "circle"},
+  {12883, "volcano"},
+};
+
+const int NUM_GATE_ROOMS = 5;
+
+
+int etherealGate(TBeing *caster, const char * portalroom, int, short bKnown)
+{
+  char buf[256];
+  TPerson *tPerson = dynamic_cast<TPerson *>(caster);
+  int i, location=0;
+  int num_charges = 20;
+  sstring sbuf;
+
+
+  for(i=0;i<NUM_GATE_ROOMS;++i){
+    if(is_abbrev(portalroom, gateRooms[i].name)){
+      location=gateRooms[i].roomnum;
+    }
+  }
+
+  TRoom * rp = real_roomp(location);
+
+  if (!rp || !location) {
+    caster->sendTo("You can't seem to open a gate to that location.\n\r");
+    return SPELL_FAIL;
+  }
+
+  if (caster->roomp->isRoomFlag(ROOM_NO_PORTAL) && !caster->isImmortal()) {
+    caster->sendTo("You can't seem to escape the defenses of this area.\n\r");
+    act("Nothing seems to happen.", FALSE, caster, NULL, NULL, TO_ROOM);
+    return SPELL_FAIL;
+  }
+
+  if (caster->bSuccess(bKnown,caster->getPerc(),SPELL_ETHER_GATE)) {
+    TPortal * tmp_obj = new TPortal(rp);
+    tmp_obj->setPortalNumCharges(num_charges);
+    tmp_obj->obj_flags.decay_time = 5;
+    tmp_obj->name = "portal gate";
+    tmp_obj->shortDescr = "<c>A <B>sw<W>ir<1><b>li<C>ng <1><c>ethereal gate<1>";
+    sbuf = format("<c>A <B>sw<W>ir<1><b>li<C>ng <1><c>ethereal gate to %s sits here.<1>") % rp->name;
+    tmp_obj->setDescr(sbuf);
+    *caster->roomp += *tmp_obj;
+
+    if (tPerson)
+      tmp_obj->checkOwnersList(tPerson);
+
+    TPortal * next_tmp_obj = new TPortal(caster->roomp);
+    next_tmp_obj->setPortalNumCharges(num_charges);
+    next_tmp_obj->obj_flags.decay_time = 5;
+    next_tmp_obj->name = "portal gate";
+    next_tmp_obj->shortDescr = "<c>A <B>sw<W>ir<1><b>li<C>ng <1><c>ethereal gate<1>";
+    sbuf = format("<c>A <B>sw<W>ir<1><b>li<C>ng <1><c>ethereal gate to %s sits here.<1>") % caster->roomp->name;
+    next_tmp_obj->setDescr(sbuf);
+    
+    *rp += *next_tmp_obj;
+
+    if (tPerson)
+      next_tmp_obj->checkOwnersList(tPerson);
+
+    caster->roomp->playsound(SOUND_SPELL_PORTAL, SOUND_TYPE_MAGIC);
+    rp->playsound(SOUND_SPELL_PORTAL, SOUND_TYPE_MAGIC);
+
+    act("$p suddenly appears out of a swirling mist.", TRUE, caster, tmp_obj, NULL, TO_ROOM);
+    act("$p suddenly appears out of a swirling mist.", TRUE, caster, tmp_obj, NULL, TO_CHAR);
+
+    sprintf(buf, "%s suddenly appears out of a swirling mist.\n\r", (sstring(next_tmp_obj->shortDescr).cap()).c_str());
+    sendToRoom(buf, location);
+
+    return SPELL_SUCCESS;
+  }
+
+  return SPELL_FAIL;
 }
 
-int ethrealGate(TBeing *caster, TObj *obj)
+int etherealGate(TBeing *caster, const char * portalroom)
 {
-  if (!bPassMageChecks(caster, SPELL_ETHER_GATE, obj))
+  if (!bPassMageChecks(caster, SPELL_ETHER_GATE, NULL))
     return FALSE;
 
   lag_t rounds = discArray[SPELL_ETHER_GATE]->lag;
   taskDiffT diff = discArray[SPELL_ETHER_GATE]->task;
 
-  start_cast(caster, NULL, obj, NULL, SPELL_ETHER_GATE, diff, 2,"", rounds, caster->in_room, 0, 0,TRUE, 0);
+  start_cast(caster, NULL, NULL, NULL, SPELL_ETHER_GATE, diff, 2, portalroom, rounds, caster->in_room, 0, 0,TRUE, 0);
   return TRUE;
 }
 
-int castEthrealGate(TBeing *caster, TObj *obj)
+int castEtherealGate(TBeing *caster, const char * portalroom)
 {
   int level = caster->getSkillLevel(SPELL_ETHER_GATE);
 
-  int ret=ethrealGate(caster,obj,level,caster->getSkillValue(SPELL_ETHER_GATE));
+  int ret=etherealGate(caster, portalroom, level, caster->getSkillValue(SPELL_ETHER_GATE));
 
   if (IS_SET(ret, SPELL_SUCCESS)) {
   }
