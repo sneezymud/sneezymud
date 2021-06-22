d95cae278ff957a17e7ebf0af2538543ef0c74bb
Durhamite2118 <69005764+Durhamite2118@users.noreply.github.com>
2021-06-21 21:39:07 -0400

    Update spec_objs.h

--- code/code/spec/spec_objs.h
+++ code/code/spec/spec_objs.h
@@ -37,7 +37,7 @@ const int SPEC_FACTIONSCORE_BOARD = 97;
 const int SPEC_GRAFFITI=135;
 const int SPEC_STOCK_BOARD  = 138;
 const int SPEC_BRICKQUEST = 148;
-const int NUM_OBJ_SPECIALS = 163;
+const int NUM_OBJ_SPECIALS = 164;
 
 struct TObjSpecs {
   bool assignable;
