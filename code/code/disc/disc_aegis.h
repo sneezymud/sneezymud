86177b469173c24256c2011adf399408027c01a4
Jeff Wolak <jeffrey.wolak@gmail.com>
2021-07-26 22:50:21 -0700

    format more

--- code/code/disc/disc_aegis.h
+++ code/code/disc/disc_aegis.h
@@ -40,83 +40,88 @@ public:
         : CDiscipline(),
         skSanctuary(), skCureParalyze(),
         skSecondWind(), skRelive(),
-        skCrusade() 
+        skCrusade()
     {
     }
 
-    CDAegis(const CDAegis &a)
+    CDAegis(const CDAegis& a)
         : CDiscipline(a),
         skSanctuary(a.skSanctuary), skCureParalyze(a.skCureParalyze),
         skSecondWind(a.skSecondWind), skRelive(a.skRelive),
-        skCrusade(a.skCrusade) 
+        skCrusade(a.skCrusade)
     {
     }
 
-    CDAegis & operator=(const CDAegis &a) 
+    CDAegis& operator=(const CDAegis& a)
     {
-      if (this == &a) 
+        if (this == &a) {
         return *this;
-      
-      CDiscipline::operator=(a),
-      skSanctuary = a.skSanctuary;
-      skCureParalyze = a.skCureParalyze;
-      skSecondWind = a.skSecondWind;
-      skRelive = a.skRelive;
-      skCrusade = a.skCrusade;
-      return *this;
+        }
+
+        CDiscipline::operator=(a),
+        skSanctuary = a.skSanctuary;
+        skCureParalyze = a.skCureParalyze;
+        skSecondWind = a.skSecondWind;
+        skRelive = a.skRelive;
+        skCrusade = a.skCrusade;
+        return *this;
     }
 
-    virtual ~CDAegis() {}
-    virtual CDAegis * cloneMe() 
-    { 
-      return new CDAegis(*this); 
+    virtual ~CDAegis()
+    {       
+    }
+
+    virtual CDAegis * cloneMe()
+    {
+        return new CDAegis(*this); 
     }
 
 private:
 };
 
-void armor(TBeing *, TBeing *);
-void armor(TBeing *, TBeing *, TMagicItem * obj, spellNumT);
-int armor(TBeing *, TBeing *, int, short, spellNumT);
+void armor(TBeing*, TBeing*);
+void armor(TBeing*, TBeing*, TMagicItem* obj, spellNumT);
+int armor(TBeing*, TBeing*, int, short, spellNumT);
 
-void sanctuary(TBeing *, TBeing *);
-void sanctuary(TBeing *, TBeing *, TMagicItem * obj);
-int sanctuary(TBeing *, TBeing *, int, short);
+void sanctuary(TBeing*, TBeing*);
+void sanctuary(TBeing*, TBeing*, TMagicItem* obj);
+int sanctuary(TBeing*, TBeing*, int, short);
 
-void bless(TBeing *, TObj *);
-void bless(TBeing *, TObj *, TMagicItem *, spellNumT);
-int bless(TBeing *, TObj *, int, short, spellNumT);
+void bless(TBeing*, TObj*);
 
-void bless(TBeing *, TBeing *);
-void bless(TBeing *, TBeing *, TMagicItem * obj, spellNumT);
-int bless(TBeing *, TBeing *, int, short, spellNumT);
+void bless(TBeing*, TObj*, TMagicItem*, spellNumT);
+int bless(TBeing*, TObj*, int, short, spellNumT);
 
-void cureBlindness(TBeing *, TBeing *);
-void cureBlindness(TBeing *, TBeing *, TMagicItem *);
-int cureBlindness(TBeing *, TBeing *, int, short);
+void bless(TBeing*, TBeing*);
+void bless(TBeing*, TBeing*, TMagicItem* obj, spellNumT);
+int bless(TBeing*, TBeing*, int, short, spellNumT);
 
-void curePoison(TBeing *, TBeing *);
-void curePoison(TBeing *, TBeing *, TMagicItem *, spellNumT);
-int curePoison(TBeing *, TBeing *, int, short, spellNumT);
+void cureBlindness(TBeing*, TBeing*);
+void cureBlindness(TBeing*, TBeing*, TMagicItem*);
+int cureBlindness(TBeing*, TBeing*, int, short);
 
-void refresh(TBeing *, TBeing *);
-void refresh(TBeing *, TBeing *, TMagicItem *, spellNumT);
-int refresh(TBeing *, TBeing *, int, short, spellNumT);
+void curePoison(TBeing*, TBeing*);
+void curePoison(TBeing*, TBeing*, TMagicItem*, spellNumT);
+int curePoison(TBeing*, TBeing*, int, short, spellNumT);
 
-void secondWind(TBeing *, TBeing *);
-void secondWind(TBeing *, TBeing *, TMagicItem *);
-int secondWind(TBeing *, TBeing *, int, short);
+void refresh(TBeing*, TBeing*);
+void refresh(TBeing*, TBeing*, TMagicItem*, spellNumT);
+int refresh(TBeing*, TBeing*, int, short, spellNumT);
 
-void cureParalysis(TBeing *, TBeing *);
-void cureParalysis(TBeing *, TBeing *, TMagicItem *);
-int cureParalysis(TBeing *, TBeing *, int, short);
+void secondWind(TBeing*, TBeing*);
+void secondWind(TBeing*, TBeing*, TMagicItem*);
+int secondWind(TBeing*, TBeing*, int, short);
 
-void cureDisease(TBeing *, TBeing *);
-int  cureDisease(TBeing *, TBeing *, int, short, spellNumT);
-void cureDisease(TBeing *, TBeing *, TMagicItem *, spellNumT);
+void cureParalysis(TBeing*, TBeing*);
+void cureParalysis(TBeing*, TBeing*, TMagicItem*);
+int cureParalysis(TBeing*, TBeing*, int, short);
 
-void relive(TBeing *, TBeing *);
+void cureDisease(TBeing*, TBeing*);
+int  cureDisease(TBeing*, TBeing*, int, short, spellNumT);
+void cureDisease(TBeing*, TBeing*, TMagicItem*, spellNumT);
 
-void crusade(TBeing *);
+void relive(TBeing*, TBeing*);
+
+void crusade(TBeing*);
 
 #endif
