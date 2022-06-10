33ad23b05a5075b130cb082d408637cf298e49d7
Drew Hershey <drew.i.hershey@gmail.com>
2022-06-10 11:35:36 -0600

    Don't handle removing AFF_ENGAGER here

--- code/code/misc/offense.cc
+++ code/code/misc/offense.cc
@@ -1019,7 +1019,7 @@ int TBeing::doFlee(const char *arg) {
     return true;
 
   // Ensure <this> no longer fighting after successful flee
-  REMOVE_BIT(specials.affectedBy, AFF_ENGAGER);
+  // Removing AFF_ENGAGER is handled within stopFighting()
   if (fight())
     stopFighting();
   if (cantHit > 0)
