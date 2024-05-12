4b29b52bccb99298530baead2e12bf54456d2239
Jeff Wolak <jeffrey.wolak@gmail.com>
2024-05-11 17:21:15 -0700

    fix manadrain

--- code/code/spec/spec_objs_manadrain.cc
+++ code/code/spec/spec_objs_manadrain.cc
@@ -8,69 +8,90 @@
 #include "being.h"
 #include "obj_base_weapon.h"
 
-/*
-  Name: Mana Drainer
-  Type: Weapon
-  Information:
-    Base chance of 1 in 51 for call.
-    Success based on level variation of user, weapon and victim.
-
-    !::number(0, max(10, (weaponLevel + (victimLevel - playerLevel))));
-    Weapon 25, Player 25, Victim 25: 0, .(1 in 26)., 25
-    Weapon 25, Player 20, Victim 30: 0, .(1 in 36)., 35
-    Weapon 25, Player 30, Victim 20: 0, .(1 in 16)., 15
-    Weapon 50, Player 50, Victim  1: 0, .(1 in 11)., 10
-    Weapon 50, Player  1, Victim 50: 0, .(1 in 75)., 74
- */
-
-int weaponManaDrainer(TBeing* tVictim, cmdTypeT tCmd, const char*, TObj* tObj,
+int weaponManaDrainer(TBeing* tVictim, cmdTypeT tCmd, const char* arg, TObj* tObj,
   TObj*) {
   TBeing* ch;
-  TBaseWeapon* tWeap;
-  bool forceSuccess = false;
+  int manaDrawn, rc;
 
-  if (!(tWeap = dynamic_cast<TBaseWeapon*>(tObj)) || !tVictim)
-    return FALSE;
+  ch = genericWeaponProcCheck(tVictim, tCmd, tObj, 3);
+  if (ch) {
+    manaDrawn = ::number(2,5);
+    act("A field of <P>energy<z> seeps from $p.", FALSE, ch, tObj, tVictim,
+      TO_CHAR);
+    act("$n braces $mself as a field of <P>energy<z> seeps from $p.", FALSE, ch,
+      tObj, tVictim, TO_NOTVICT);
+    act(
+      "You feel your energy sucked out of you as a field of <P>energy<z> seeps "
+      "from $n's $p.",
+      FALSE, ch, tObj, tVictim, TO_VICT);
+
+    tVictim->addToMana(-manaDrawn);
+    ch->addToMana(manaDrawn);
+    rc = ch->reconcileDamage(tVictim, manaDrawn, DAMAGE_ELECTRIC);
+    if (IS_SET_DELETE(rc, DELETE_VICT))
+      return DELETE_VICT;
+    return TRUE;
+  }
 
   if (!(ch = dynamic_cast<TBeing*>(tObj->equippedBy)))
     return FALSE;
 
-  if (ch->getName() == "Lapsos" && ch->isImmortal())
-    forceSuccess = true;
+  if (tCmd == CMD_SAY || tCmd == CMD_SAY2) {
+    sstring buf, buf2;
+    TBeing* vict = NULL;
+    buf = sstring(arg).word(0);
+    buf2 = sstring(arg).word(1);
 
-  if (::number(0, 50) && !forceSuccess)
-    return FALSE;
+    if (buf == "feed" && buf2 == "me") {
+      if (ch->checkObjUsed(tObj)) {
+        act("You cannot use $p's powers again this soon.", TRUE, ch, tObj, NULL,
+          TO_CHAR, NULL);
+        return FALSE;
+      }
 
-  if (tCmd != CMD_OBJ_HITTING)
-    return FALSE;
+      if (!(vict = ch->fight())) {
+        act("You cannot use $p's powers unless you are fighting.", TRUE, ch, tObj,
+          NULL, TO_CHAR, NULL);
+        return FALSE;
+      }
 
-  if ((!tVictim->hasClass(CLASS_MAGE) && !tVictim->hasClass(CLASS_RANGER) &&
-        !tVictim->hasClass(CLASS_MONK)) ||
-      (!ch->hasClass(CLASS_MAGE) && !ch->hasClass(CLASS_RANGER) &&
-        !ch->hasClass(CLASS_MONK)))
-    return FALSE;
+      ch->addObjUsed(tObj, Pulse::UPDATES_PER_MUDHOUR);
 
-  if (!::number(0,
-        std::max(10, (int)(tWeap->weaponLevel() +
-                           (tVictim->GetMaxLevel() - ch->GetMaxLevel())))) ||
-      forceSuccess) {
-    act("A field of darkness seeps from $p.", FALSE, ch, tObj, tVictim,
-      TO_CHAR);
-    act("$n braces $mself as a field of darkness seeps from $p.", FALSE, ch,
-      tObj, tVictim, TO_NOTVICT);
-    act(
-      "You feel your energy sucked out of you as a field of darkness seeps "
-      "from $n's $p.",
-      FALSE, ch, tObj, tVictim, TO_VICT);
+      act("$n's $o glows <P>a bright purple<1> as $e chants a <Y>word of power<1>.",
+        TRUE, ch, tObj, NULL, TO_ROOM, NULL);
+      act("$n steps back and points $p at $N!<1>", TRUE, ch, tObj, vict,
+        TO_NOTVICT, NULL);
+      act(
+        "<b>A violent ripple of magic shoots from $n's <c>$o<b>, and strikes $N "
+        "in the chest!<1>",
+        TRUE, ch, tObj, vict, TO_NOTVICT, NULL);
+      act("$n braces their $p and points it at you! Uh oh!<1>", TRUE, ch, tObj, vict,
+        TO_VICT, NULL);
+      act(
+        "<b>You feel your soul being yanked by the ray coming from $n's <c>$o<b>",
+        TRUE, ch, tObj, vict, TO_VICT, NULL);
+      act("Your $o emits a <P>bright purple<z> as you chant, '<p>feed me!<1>'.",
+        TRUE, ch, tObj, NULL, TO_CHAR, NULL);
+      act("You brace yourself and point $p at $N!<1>", TRUE, ch, tObj, vict, TO_CHAR,
+        NULL);
+      act(
+        "<b>A violent ripple erupts from your <c>$o<b> and strikes $N "
+        "in the chest!<1>",
+        TRUE, ch, tObj, vict, TO_CHAR, NULL);
 
-    int manaDrawn = ::number(1,
-      std::max(2, std::min(10, (ch->GetMaxLevel() - tVictim->GetMaxLevel()))));
+      manaDrawn= ::number(10, 60);
+          
+      tVictim->addToMana(-manaDrawn);
+      ch->addToMana(manaDrawn);
+      ch->gainCondition(FULL, 15);
+      ch->gainCondition(THIRST, 15);
 
-    tVictim->addToMana(-manaDrawn);
-    ch->addToMana(manaDrawn);
-
-    return TRUE;
+      rc = ch->reconcileDamage(vict, manaDrawn, DAMAGE_ELECTRIC);
+      if (IS_SET_DELETE(rc, DELETE_VICT))
+        return DELETE_VICT;
+      return TRUE;
+    }
   }
-
   return FALSE;
 }
+
