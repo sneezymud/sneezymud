1a27311402a9c17ea5368f187ee78379cdadb031
Drew Hershey <7330404+drewhershey@users.noreply.github.com>
2025-12-15 09:59:25 -0700

    Address CRabbit PR feedback
    - Add description to install-build-deps action
    - Fix tilde expansion in setup-cmake-cache action
    - Update CMake minimum to 3.25 (required for preset v6)
    - Add comment about GLOB reconfigure requirement
    - Fix UB in getRandomEntryByVal/ByRef for empty vectors
    - Quote paths in dump-databases.sh
    - Make killall non-fatal in verify_boot.sh

--- scripts/dump-databases.sh
+++ scripts/dump-databases.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-pushd ~/sneezymud-docker/sneezymud/_Setup-data/sql_data
+pushd "${HOME}/sneezymud-docker/sneezymud/_Setup-data/sql_data"
 for database in *; do
-	pushd $database
+	pushd "$database"
 	for i in *; do
-		table=`echo $i | sed s/.sql//`
+		table=$(echo "$i" | sed 's/.sql//')
 		# Use mariadb-dump for consistency with modern MariaDB containers
 		# Remove timestamp comments to avoid unnecessary diffs
-		docker exec sneezy-db mariadb-dump -h sneezy-db -u sneezy --password=password $database $table | \
+		docker exec sneezy-db mariadb-dump -h sneezy-db -u sneezy --password=password "$database" "$table" | \
 			sed 's/),(/),\n(/g' | \
-			grep -v '^-- Dump completed on' > $i
+			grep -v '^-- Dump completed on' > "$i"
 	done
 	popd
 done
